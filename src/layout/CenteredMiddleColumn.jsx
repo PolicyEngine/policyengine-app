d6161ba30df894495838926208af8110d7d8ce39
Nikhil Woodruff <nikhil@policyengine.org>
2022-11-29 17:58:21 +0000

    Center splash screen

--- src/layout/CenteredMiddleColumn.jsx
+++ src/layout/CenteredMiddleColumn.jsx
@@ -7,11 +7,12 @@ export default function CenteredMiddleColumn(props) {
         flexDirection: "column",
         alignItems: "center",
         marginTop: marginTop || "20%",
+        justifyContent: "center",
       }}
     >
-      <h1 style={{ marginBottom: marginBottom || 20 }}>{title}</h1>
+      <h1 style={{ marginBottom: marginBottom || 20, textAlign: "center" }}>{title}</h1>
       {description && (
-        <h4 style={{ paddingLeft: 100, paddingRight: 100 }}>{description}</h4>
+        <h4 style={{ paddingLeft: 100, paddingRight: 100, textAlign: "center" }}>{description}</h4>
       )}
       {children}
     </div>
