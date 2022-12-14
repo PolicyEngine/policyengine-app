329b803526af529beba44f7763d03f4503decbc2
Nikhil Woodruff <nikhil@policyengine.org>
2022-12-14 18:38:09 +0000

    Attempt to fix white screens

--- gcp/main.py
+++ gcp/main.py
@@ -2,6 +2,18 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='build')
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
