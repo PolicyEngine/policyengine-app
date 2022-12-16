1fc0a2f33a73e1bce576961b4ad31ab325807a94
Nikhil Woodruff <nikhil@policyengine.org>
2022-12-16 22:17:56 +0000

    Add Google Analytics

--- src/PolicyEngine.jsx
+++ src/PolicyEngine.jsx
@@ -6,12 +6,14 @@ import {
   Navigate,
 } from "react-router-dom";
 import PolicyEngineCountry from "./PolicyEngineCountry";
+import ReactGA from "react-ga";
 
 export default function PolicyEngine() {
 
   // Look up the country ID from the user's browser language
   const browserLanguage = navigator.language;
   const countryId = browserLanguage === "en-US" ? "us" : "uk";
+  ReactGA.initialize("G-91M4529HE7");
   return (
     <Router>
       <Routes>
