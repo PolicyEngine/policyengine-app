ef025b9e45fb01ebaf82ce25bf400061cc41a86b
Nikhil Woodruff <nikhil@policyengine.org>
2022-12-16 23:09:42 +0000

    Fix search field

--- src/controls/SearchOptions.jsx
+++ src/controls/SearchOptions.jsx
@@ -6,16 +6,13 @@ export default function SearchOptions(props) {
     props;
   const [value, setValue] = useState(defaultValue);
   const [searchText, setSearchText] = useState("");
-  const filteredOptions = options.filter((option) =>
-    option.label.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-  // Shouldn't allow selection of an option that doesn't exist in the options list.
+  const filteredOptions = options;
 
   return (
     <Select
       showSearch
       options={filteredOptions}
+      optionFilterProp="children"
       onSelect={(value) => {
         if (options.find((option) => option.value === value)) {
           setValue(value);
@@ -30,6 +27,9 @@ export default function SearchOptions(props) {
           onSearch(text);
         }
       }}
+      filterOption={(input, option) =>
+        (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+      }
       style={style || { width: 200, marginLeft: 10, marginRight: 10 }}
       placeholder={
         value
