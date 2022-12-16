d993e538cd7d27e49a7fb443c366bee440254f7d
Nikhil Woodruff <nikhil@policyengine.org>
2022-12-16 12:01:55 +0000

    Automatically set country

--- src/PolicyEngine.jsx
+++ src/PolicyEngine.jsx
@@ -8,12 +8,18 @@ import {
 import PolicyEngineCountry from "./PolicyEngineCountry";
 
 export default function PolicyEngine() {
+
+  // Look up the country ID from the user's browser language
+  const browserLanguage = navigator.language;
+  const countryId = browserLanguage === "en-US" ? "us" : "uk";
   return (
     <Router>
       <Routes>
         <Route path="/uk/*" element={<PolicyEngineCountry countryId="uk" />} />
         <Route path="/us/*" element={<PolicyEngineCountry countryId="us" />} />
-        <Route exact path="/" element={<Navigate to="/uk" />} />
+        <Route exact path="/" element={
+          <Navigate to={`/${countryId}`} />
+        } />
       </Routes>
     </Router>
   );
