3360f675eb03cf1526ef6ba2948cb17a45519d80
Max Ghenis <mghenis@gmail.com>
2023-03-26 15:46:40 -0400

    ELI5 and Wonk modes for GPT analysis Fixes #374

--- src/pages/policy/output/Prompt.jsx
+++ src/pages/policy/output/Prompt.jsx
@@ -12,7 +12,7 @@ export default function Prompt(props) {
   const selectedVersion = searchParams.get("version") || metadata.version;
   const relevantParameters = Object.keys(policy.reform.data).map(parameter => metadata.parameters[parameter]);
   // metadata.economy_options.region = [{name: "uk", label: "United Kingdom"}]
-  const regionKeyToLabel = metadata.economy_options.region.reduce((acc, {name, label}) => {
+  const regionKeyToLabel = metadata.economy_options.region.reduce((acc, { name, label }) => {
     acc[name] = label;
     return acc;
   }, {});
@@ -49,7 +49,17 @@ export default function Prompt(props) {
   This JSON describes the baseline and reform poverty and deep poverty rates by gender (briefly describe the relative changes): ${JSON.stringify(impact.poverty_by_gender)}
 
   This JSON describes three inequality metrics in the baseline and reform, the Gini coefficient of income inequality, the share of income held by the top 10% of households and the share held by the top 1% (describe the relative changes): ${JSON.stringify(impact.inequality)}`;
-  const analysisPrompt = policyDetails + description;
+
+  const [mode, setMode] = useState("Normal");
+
+  const modePrompts = {
+    ELI5: "Explain like I'm 5: ",
+    Normal: "",
+    Wonk: "Explain like I'm a policy wonk seeking a technical brief (you can use acronyms and other jargon): ",
+  };
+
+  const analysisPrompt = modePrompts[mode] + policyDetails + description;
+
   const [analysis, setAnalysis] = useState("");
   const [loading, setLoading] = useState(false);
   const [hasClickedGenerate, setHasClickedGenerate] = useState(false);
@@ -61,12 +71,43 @@ export default function Prompt(props) {
   const [showPrompt, setShowPrompt] = useState(false);
   const lines = analysisPrompt.split("\n");
 
+  // Add a function to handle mode changes
+  const handleModeChange = (modeValue) => {
+    setMode(modeValue);
+  };
+
+
+  const buttonWidth = "80px";
+  const activeColor = "#6c757d";
+  const inactiveColor = "white";
+  const borderColor = "1px solid #6c757d";
+
+  const ModeButton = ({ modeValue, currentMode, handleModeChange }) => (
+    <button
+      style={{
+        backgroundColor: modeValue === currentMode ? activeColor : inactiveColor,
+        color: modeValue === currentMode ? inactiveColor : activeColor,
+        borderRadius: modeValue === "ELI5" ? "5px 0 0 5px" : modeValue === "Wonk" ? "0 5px 5px 0" : 0,
+        border: borderColor,
+        borderRight: modeValue !== "Wonk" ? "none" : borderColor,
+        padding: "5px 10px",
+        margin: 0,
+        cursor: "pointer",
+        width: buttonWidth,
+      }}
+      onClick={() => handleModeChange(modeValue)}
+    >
+      {modeValue}
+    </button>
+  );
+
+
   const onGenerate = () => {
     setHasClickedGenerate(true);
     setLoading(true);
     openai.createChatCompletion({
       model: "gpt-4",
-      messages: [{role: "user", content: analysisPrompt}],
+      messages: [{ role: "user", content: analysisPrompt }],
     }).then((response) => {
       console.log(response);
       setAnalysis(response.data.choices[0].message.content);
@@ -79,31 +120,68 @@ export default function Prompt(props) {
     return <p key={i}>{line}</p>;
   });
   const buttonText =
-    !hasClickedGenerate ? 
-      "Generate an analysis" : 
-      loading ? 
-        <><Spinner style={{marginRight: 10}} />Generating</> : 
+    !hasClickedGenerate ?
+      "Generate an analysis" :
+      loading ?
+        <><Spinner style={{ marginRight: 10 }} />Generating</> :
         "Regenerate analysis";
+
+
   return (
     <>
       <h2>Analysis</h2>
-      <p>PolicyEngine&apos;s automatic policy analysis is powered by GPT-4. Read more about this feature <a href="https://policyengine.org/uk/blog/2023-03-17-automate-policy-analysis-with-policy-engines-new-chatgpt-integration">here</a>. Generation usually takes around 20 seconds. This is an experimental feature, so please verify the claims made against the prompt.</p>
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        marginBottom: 20,
-      }}>
-        <Button primary text={buttonText} onClick={onGenerate} style={{maxWidth: 250}} />
+      <p>
+        PolicyEngine&apos;s automatic policy analysis is powered by GPT-4. Read
+        more about this feature{" "}
+        <a href="https://policyengine.org/uk/blog/2023-03-17-automate-policy-analysis-with-policy-engines-new-chatgpt-integration">
+          here
+        </a>
+        . Generation usually takes around 20 seconds. This is an experimental
+        feature, so please verify the claims made against the prompt.
+      </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: 20,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: 20,
+          }}
+        >
+          <ModeButton
+            modeValue="ELI5"
+            currentMode={mode}
+            handleModeChange={handleModeChange}
+          />
+          <ModeButton
+            modeValue="Normal"
+            currentMode={mode}
+            handleModeChange={handleModeChange}
+          />
+          <ModeButton
+            modeValue="Wonk"
+            currentMode={mode}
+            handleModeChange={handleModeChange}
+          />
+        </div>
+        <Button primary text={buttonText} onClick={onGenerate} style={{ maxWidth: 250 }} />
+        {
+          !hasClickedGenerate ?
+            null :
+            loading ?
+              null :
+              analysisContent
+        }
       </div>
-      {
-        !hasClickedGenerate ? 
-          null : 
-          loading ? 
-            null : 
-            analysisContent
-      }
       <div style={{
         display: "flex",
         flexDirection: "column",
@@ -117,7 +195,7 @@ export default function Prompt(props) {
             "Show prompt"
         } onClick={
           () => setShowPrompt(!showPrompt)
-        } style={{maxWidth: 250}} />
+        } style={{ maxWidth: 250 }} />
       </div>
       {
         showPrompt ?
@@ -145,7 +223,7 @@ export default function Prompt(props) {
           </> :
           null
       }
-      
+
     </>
   );
 }
