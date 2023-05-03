fb2e13675e1e1a8fdc949f9e429d5f28db98b8fb
Christina Li <christinalee.is@gmail.com>
2023-05-03 09:22:43 -0700

    Updated on the Subscribe Button Link

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -364,83 +364,93 @@ function SocialMediaIcons(props) {
 }
 
 //CHANGES HERE 
+//CHANGES HERE 
 function SubscribeForm() {
     
 
-        const [searchTerm, setSearchTerm] = useState('');
-        const [subscribed, setSubscribed] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [subscribed, setSubscribed] = useState(false);
 
 
-        const handleSubscribe = (e) => {
-            e.preventDefault();
-            setSubscribed(true);
-        };
-        
-        return (
+    const handleSubscribe = (e) => {
+        e.preventDefault();
+        setSubscribed(true);
+    };
 
-
-    <div className="Subscribe"
-            style={{
-            aspectratio: 4/3,
-            padding: 60, 
-            textAlign: "center",
-            backgroundColor: '#F4F8FB',
-            opacity: 70,
-            borderRadius: '7px',
-
-            }}>
-    <h2
-        style={{
-            color:'black',
-            fontSize: 35,
-            marginBottom: 3,
-            }}>
-            <b>Subscribe to Policy Engine</b>
-            </h2>
-            <p
-        style={{
-            color:'grey',
-            fontSize: 22,
-            marginTop: 3,
-            }}>
-            Get the latest posts delivered right to your inbox</p>
-            <form onSubmit={handleSubscribe}>
-    <input
-        type="text"
-        style={{
-            padding:10,
-            width: 300,
-            fontSize: 15,
-            maxWidth: '300px',
-            boxSizing: 'border-box',
-        }}
-        placeholder="youremail@example.com"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-    />
     
-    <a href="https://policyengine.us5.list-manage.com/subscribe/post?u=e5ad35332666289a0f48013c5&amp;id=71ed1f89d8&amp;f_id=00f173e6f0">
-    <button className='bluebutton'
-        value={{subscribed}}
+    return (
+
+<div className="Subscribe"
         style={{
-            marginLeft:10,
-            padding:10,
-            color: 'black',
-            backgroundColor: '#B4E5FF',
-            boxShadow: 'none',
-            boxSizing: 'border-box',
-        }}
-        type="submit"
-        >
-        Subscribe
-    </button>
-    </a>
-    </form>
+        padding: '50px', 
+        textAlign: "center",
+        backgroundColor: '#D2D2D2',
+        opacity: 70,
+        borderRadius: '7px',
+        width: '100%',
+        }}>
+<h2
+    style={{
+        color:'black',
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginBottom: 3,
+        }}>
+        Subscribe to Policy Engine
+        </h2>
+        <h3
+    style={{
+        color:'black',
+        fontSize: 18,
+        marginTop: 3,
+        paddingBottom: 10,
+        }}>
+        Get the latest posts delivered right to your inbox</h3>
+        <form onSubmit={handleSubscribe}>
+<input
+    type="text"
+    style={{
+        padding:10,
+        width: 300,
+        fontSize: 15,
+        maxWidth: '300px',
+        borderWidth: 0,
+        outline:'none',
+        boxShadow: 'none',
+        boxSizing: 'border-box',
+    }}
+    placeholder="youremail@example.com"
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+/>
 
 
-    </div>
+<button className='bluebutton'
+    value={{subscribed}}
+    style={{
+        marginLeft:10,
+        padding:10,
+        color: 'white',
+        backgroundColor: '#2C6496',
+        borderWidth: 0,
+        boxShadow: 'none',
+        boxSizing: 'border-box',
+    }}
+    onClick={() => {
+        window.open('https://policyengine.us5.list-manage.com/subscribe/post?u=e5ad35332666289a0f48013c5&id=71ed1f89d8&f_id=00f173e6f0', '_blank');
+    }}
+    type="submit"
+    >
+    <b>Subscribe</b>
+</button>
+
+</form>
+
+
+</div>
 );
 }
+
 
 
 
