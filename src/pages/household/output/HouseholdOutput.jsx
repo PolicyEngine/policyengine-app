964545f92cee78260a9e0d33f4a9a70a2b4dd189
Patrick Hanley <patrickhanl@gmail.com>
2023-06-17 08:51:41 -0700

    fixed using baseline and reform labels for policy label

--- src/pages/household/output/HouseholdOutput.jsx
+++ src/pages/household/output/HouseholdOutput.jsx
@@ -45,7 +45,7 @@ export default function HouseholdOutput(props) {
   if (reformLabel !== "Current law" && baselineLabel === "Current law") {
     policyLabel = reformLabel;
   } else {
-    policyLabel = `${reformLabel} (compared against ${reformLabel})`;
+    policyLabel = `${reformLabel} (compared against ${baselineLabel})`;
   }
   let pane;
   if (loading) {
