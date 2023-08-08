bbdf588e7fd98a7f37452c3c71b9e73d2df278c4
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-08-08 14:24:55 +0100

    Revert back to main site

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.min.css";
-import PolicyEngine from "./redesign/PolicyEngine";
+import PolicyEngine from "./PolicyEngine";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
