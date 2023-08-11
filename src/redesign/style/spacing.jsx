b3570f45f45ba3b9761d6a48d390218a44af2acc
Anthony Volk <anth.volk@gmail.com>
2023-08-11 15:21:53 -0600

    feat: Added px-sized spacing constants

--- src/redesign/style/spacing.jsx
+++ src/redesign/style/spacing.jsx
@@ -1,7 +1,31 @@
 export const HEADER_HEIGHT = 90;
 
+const desktopPadding = {
+	top: '120px',
+	bottom: '160px',
+	left: '188px',
+	right: '188px'
+};
+
+const tabletPadding = {
+	top: '80px',
+	bottom: '100px',
+	left: '64px',
+	right: '64px'
+};
+
+const mobilePadding = {
+	top: '48px',
+	bottom: '60px',
+	left: '34px',
+	right: '34px'
+};
+
 const spacing = {
   HEADER_HEIGHT,
+  desktopPadding,
+  tabletPadding,
+  mobilePadding
 };
 
 export default spacing;
