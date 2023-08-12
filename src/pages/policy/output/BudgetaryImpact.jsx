773fa60470d7feb9b90f736188dc42de88fb8e02
jacky5124 <jacky5124@gmail.com>
2023-08-12 05:11:36 -0700

    added "hovertemplate" for BudgetaryImpact plots

--- src/pages/policy/output/BudgetaryImpact.jsx
+++ src/pages/policy/output/BudgetaryImpact.jsx
@@ -48,15 +48,18 @@ export default function BudgetaryImpact(props) {
   const screenshotRef = useRef();
   console.log(values, labels, valuesBeforeFilter, labelsBeforeFilter);
 
-  function BudgetaryImpactPlot() {
+  function BudgetaryImpactPlot(props) {
     const setHoverCard = useContext(HoverCardContext);
+    const { useHoverCard = false } = props;
+    const xArray = labels.length > 0 ? labels : ["Net impact"];
+    const yArray = labels.length > 0 ? values : [budgetaryImpact / 1e9];
     // Waterfall chart
     return (
       <Plot
         data={[
           {
-            x: labels.length > 0 ? labels : ["Net impact"],
-            y: labels.length > 0 ? values : [budgetaryImpact / 1e9],
+            x: xArray,
+            y: yArray,
             type: "waterfall",
             orientation: "v",
             // 'relative' for all but the last, which is 'total'
@@ -82,7 +85,42 @@ export default function BudgetaryImpact(props) {
               },
             },
             connector: { line: { color: style.colors.DARK_GRAY } },
-            hoverinfo: "none",
+            ...(useHoverCard
+              ? {
+                  hoverinfo: "none",
+                }
+              : {
+                  customdata: xArray.map((x, i) => {
+                    const label = x;
+                    // look up label/values array
+                    let relevantFigure = yArray[i] * 1e9;
+                    // If tax revenues, negate
+                    if (label.toLowerCase().includes("tax")) {
+                      relevantFigure = -relevantFigure;
+                    }
+                    let body =
+                      relevantFigure < 0
+                        ? `This reform would increase<br>`
+                        : relevantFigure > 0
+                        ? `This reform would reduce<br>`
+                        : `This reform would not impact<br>`;
+                    body += label.toLowerCase().includes("tax")
+                      ? label.toLowerCase()
+                      : label.toLowerCase().includes("benefit")
+                      ? "benefit spending"
+                      : "the budget deficit";
+                    if (relevantFigure === 0) {
+                      body += ".";
+                    } else {
+                      body += ` by ${aggregateCurrency(
+                        Math.abs(relevantFigure),
+                        metadata,
+                      )}.`;
+                    }
+                    return body;
+                  }),
+                  hovertemplate: `<b>%{x}</b><br><br>%{customdata}<extra></extra>`,
+                }),
           },
         ]}
         layout={{
@@ -94,6 +132,15 @@ export default function BudgetaryImpact(props) {
             tickprefix: metadata.currency,
             tickformat: ",.1f",
           },
+          ...(useHoverCard
+            ? {}
+            : {
+                hoverlabel: {
+                  align: "left",
+                  bgcolor: "#FFF",
+                  font: { size: "16" },
+                },
+              }),
           uniformtext: {
             mode: "hide",
             minsize: 12,
@@ -114,41 +161,45 @@ export default function BudgetaryImpact(props) {
         style={{
           width: "100%",
         }}
-        onHover={(data) => {
-          const label = data.points[0].x;
-          // look up label/values array
-          let relevantFigure = values[labels.indexOf(label)] * 1e9;
-          // If tax revenues, negate
-          if (label.toLowerCase().includes("tax")) {
-            relevantFigure = -relevantFigure;
-          }
-          let body =
-            relevantFigure < 0
-              ? "This reform would increase "
-              : relevantFigure > 0
-              ? "This reform would reduce "
-              : "This reform would not impact ";
-          body += label.toLowerCase().includes("tax")
-            ? label.toLowerCase()
-            : label.toLowerCase().includes("benefit")
-            ? "benefit spending"
-            : "the budget deficit";
-          if (relevantFigure === 0) {
-            body += ".";
-          } else {
-            body += ` by ${aggregateCurrency(
-              Math.abs(relevantFigure),
-              metadata,
-            )}.`;
-          }
-          setHoverCard({
-            title: label,
-            body: body,
-          });
-        }}
-        onUnhover={() => {
-          setHoverCard(null);
-        }}
+        {...(useHoverCard
+          ? {
+              onHover: (data) => {
+                const label = data.points[0].x;
+                // look up label/values array
+                let relevantFigure = values[labels.indexOf(label)] * 1e9;
+                // If tax revenues, negate
+                if (label.toLowerCase().includes("tax")) {
+                  relevantFigure = -relevantFigure;
+                }
+                let body =
+                  relevantFigure < 0
+                    ? "This reform would increase "
+                    : relevantFigure > 0
+                    ? "This reform would reduce "
+                    : "This reform would not impact ";
+                body += label.toLowerCase().includes("tax")
+                  ? label.toLowerCase()
+                  : label.toLowerCase().includes("benefit")
+                  ? "benefit spending"
+                  : "the budget deficit";
+                if (relevantFigure === 0) {
+                  body += ".";
+                } else {
+                  body += ` by ${aggregateCurrency(
+                    Math.abs(relevantFigure),
+                    metadata,
+                  )}.`;
+                }
+                setHoverCard({
+                  title: label,
+                  body: body,
+                });
+              },
+              onUnhover: () => {
+                setHoverCard(null);
+              },
+            }
+          : {})}
       />
     );
   }
