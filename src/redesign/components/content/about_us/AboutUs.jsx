8f5098a2d9c4e0d24c90d21fe802e3afe32841cc
Anthony Volk <anth.volk@gmail.com>
2023-08-13 20:54:58 -0600

    fix: Added document title for about page

--- src/redesign/components/content/about_us/AboutUs.jsx
+++ src/redesign/components/content/about_us/AboutUs.jsx
@@ -4,6 +4,8 @@ import StaffSection from './subcomponents/StaffSection.jsx';
 // This layout component contains the entirety of the About Us page
 export default function AboutUs() {
 
+	document.title = "About | PolicyEngine";
+
 	return (
 		<StaffSection />
 	)
