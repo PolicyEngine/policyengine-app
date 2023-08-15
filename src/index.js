873c14a6079bafa68df3e80a4072b58b1a682469
Anthony Volk <anth.volk@gmail.com>
2023-08-14 21:23:31 -0600

    fix: Return index.js to normal state

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.min.css";
-// import PolicyEngine from "./redesign/PolicyEngine";
-import TitleAndDescription from "redesign/components/content/TitleAndDescription.jsx";
+import PolicyEngine from "./redesign/PolicyEngine";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TitleAndDescription title={'Test Title'} description={'Explore Brookings’ research and commentary to deepen your understanding of local, national, and global challenges. Our experts offer evidence-based analysis and innovative policy solutions that inform decision-making and drive positive change.'}/>);
+root.render(<PolicyEngine />);
 
 serviceWorkerRegistration.unregister();
