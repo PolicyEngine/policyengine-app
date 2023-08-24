6eec78235a9dd5285ba4758ca237bf87788bfbb9
Nicholas Ung <ung.nicholask@gmail.com>
2023-08-23 22:30:18 -0700

    Add title description on Donate page

--- src/redesign/components/Donate.jsx
+++ src/redesign/components/Donate.jsx
@@ -8,7 +8,10 @@ export default function Donate() {
   return (
     <div>
       <Header />
-      <PageHeader title="Donate" />
+      <PageHeader title="Donate">
+          Explore Brookings’ research and commentary to deepen your understanding of local, national, and global challenges. 
+          Our experts offer evidence-based analysis and innovative policy solutions that inform decision-making and drive positive change.
+      </PageHeader>
       <Section
         height={400}
         backgroundColor={style.colors.BLUE_PRIMARY}
