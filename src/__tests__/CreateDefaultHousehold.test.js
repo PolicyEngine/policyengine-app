f97e31aa761835a19d4aec837e80e2cac01487a0
Anthony Volk <anth.volk@gmail.com>
2023-09-05 18:57:53 -0600

    fix: Corrected CreateDefaultHousehold test

--- src/__tests__/CreateDefaultHousehold.test.js
+++ src/__tests__/CreateDefaultHousehold.test.js
@@ -5,25 +5,6 @@ import { defaultHouseholds } from "data/defaultHouseholds.js";
 function constructTestSituationUS() {
   let testHousehold = JSON.parse(JSON.stringify((defaultHouseholds.us)));
 
-  // Add state_name to households
-  testHousehold.households["your household"] = {
-    ...testHousehold.households["your household"],
-    state_name: {
-      2023: null
-    }
-  };
-
-  // Add employment_income and age to each person
-  testHousehold.people.you = {
-    ...testHousehold.people.you,
-    employment_income: {
-      2023: 0
-    },
-    age: {
-      2023: 40
-    }
-  };
-
   return testHousehold;
 }
 
