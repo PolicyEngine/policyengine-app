161f8bc2ebb71888bd2e711ea09732a3daa5814a
Nicholas Ung <nicholasung22@gmail.com>
2023-10-11 18:18:58 -0700

    Add title to author data
    Signed-off-by: Nicholas Ung <nicholasung22@gmail.com>

--- src/redesign/data/Authors.jsx
+++ src/redesign/data/Authors.jsx
@@ -7,7 +7,8 @@ const Authors = {
       "github": "https://github.com/nikhilwoodruff",
       "linkedin": "https://www.linkedin.com/in/nikhil-woodruff/",
       "headshot": require("../../images/authors/nikhil-woodruff.jpg"),
-      "bio": "is PolicyEngine's co-founder and CTO."
+      "bio": "is PolicyEngine's co-founder and CTO.",
+      "title": "PolicyEngine's Co-founder and CTO"
     },
     "max-ghenis": {
       "name": "Max Ghenis",
@@ -16,7 +17,8 @@ const Authors = {
       "github": "https://github.com/MaxGhenis",
       "linkedin": "https://www.linkedin.com/in/maxghenis/",
       "headshot": require("../../images/authors/max-ghenis.jpg"),
-      "bio": "is PolicyEngine's co-founder and CEO."
+      "bio": "is PolicyEngine's co-founder and CEO.",
+      "title": "PolicyEngine's Co-founder and CEO"
     },
     "kevin-foster": {
       "name": "Kevin Foster",
@@ -24,7 +26,8 @@ const Authors = {
       "github": "https://github.com/kfoster19",
       "linkedin": "https://www.linkedin.com/in/kevin-h-foster/",
       "headshot": require("../../images/authors/kevin-foster.jpeg"),
-      "bio": "is a researcher at PolicyEngine."
+      "bio": "is a researcher at PolicyEngine.",
+      "title": "Researcher at PolicyEngine"
     },
     "nicholas-rodelo": {
       "name": "Nicholas Rodelo",
@@ -33,12 +36,14 @@ const Authors = {
       "linkedin": "https://www.linkedin.com/in/nicholas-rodelo-aaaa18159/",
       "twitter": "https://twitter.com/NicholasRodelo",
       "headshot": require("../../images/authors/nicholas-rodelo.jpg"),
-      "bio": "is a researcher at PolicyEngine."
+      "bio": "is a researcher at PolicyEngine.",
+      "title": "Researcher at PolicyEngine"
     },
     "chat-gpt": {
       "name": "ChatGPT",
       "headshot": require("../../images/authors/chat-gpt.png"),
-      "bio": "is a contributing writer at PolicyEngine"
+      "bio": "is a contributing writer at PolicyEngine.",
+      "title": "Contributing writer at PolicyEngine"
     },
     "arthur-wright": {
       "name": "Arthur Wright",
@@ -47,6 +52,7 @@ const Authors = {
       "github": "awrigh46",
       "headshot": require("../../images/authors/arthur-wright.jpg"),
       "bio": "is a researcher at PolicyEngine.",
+      "title": "Researcher at PolicyEngine",
     },
     "donglai-xu": {
       "name": "Donglai Xu",
@@ -54,7 +60,8 @@ const Authors = {
       "github": "xudonglai0426",
       "linkedin": "https://www.linkedin.com/in/donglaixu11101",
       "headshot": require("../../images/authors/donglai-xu.jpeg"),
-      "bio": "is a data analyst at PolicyEngine."
+      "bio": "is a data analyst at PolicyEngine.",
+      "title": "Data Analyst at PolicyEngine"
     },
     "lin-tao": {
       "name": "Lin Tao",
@@ -62,7 +69,8 @@ const Authors = {
       "github": "Kim1277",
       "linkedin": "https://www.linkedin.com/in/lin-tao-384203207",
       "headshot": require("../../images/authors/lin-tao.jpeg"),
-      "bio": "is a data analyst at PolicyEngine."
+      "bio": "is a data analyst at PolicyEngine.",
+      "title": "Data Analyst at PolicyEngine"
     }
   }
 
