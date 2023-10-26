3a5371d52fc30f19d51471a79ea5bd57c2d9dbcf
Nicholas Ung <nicholasung22@gmail.com>
2023-10-26 00:29:13 -0700

    Adjust width parameters to enable hover effect

--- src/redesign/components/Donate.jsx
+++ src/redesign/components/Donate.jsx
@@ -8,7 +8,9 @@ import ActionButton from "./ActionButton";
 
 export default function Donate() {
   const displayCategory = useDisplayCategory();
-
+  const mobile = displayCategory === "mobile";
+  const desktop = displayCategory === "desktop";
+  
   const handleButtonClick = () => {
     window.open("https://opencollective.com/psl-foundation", "_blank");
   };
@@ -72,7 +74,7 @@ export default function Donate() {
                 </div>
               }
               onClick={handleButtonClick}
-              width={displayCategory === "mobile" ? "420px" : "130%"}
+              width={desktop ? 450 : mobile ? "70vw" : "30vw"}
               noArrow={true}
             />
           </div>
