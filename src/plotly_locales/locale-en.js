15bd8515562c3b106b973c24c5276243de81936b
Abhishek Singh <31144719+abhcs@users.noreply.github.com>
2023-10-27 10:34:47 -0500

    re-register locales en and en-us with Plotly

---
+++ src/plotly_locales/locale-en.js
@@ -0,0 +1,63 @@
+// same as plotly.js/src/locale-en.js except currency symbol
+
+"use strict";
+
+module.exports = {
+  moduleType: "locale",
+  name: "en",
+  dictionary: {
+    "Click to enter Colorscale title": "Click to enter Colourscale title",
+  },
+  format: {
+    days: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+    shortDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    months: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    shortMonths: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    periods: ["AM", "PM"],
+    dateTime: "%a %b %e %X %Y",
+    date: "%d/%m/%Y",
+    time: "%H:%M:%S",
+    decimal: ".",
+    thousands: ",",
+    grouping: [3],
+    currency: ["£", ""],
+    year: "%Y",
+    month: "%b %Y",
+    dayMonth: "%b %-d",
+    dayMonthYear: "%b %-d, %Y",
+  },
+};
