6d901a85b82c049d411455383a272ac7053c8893
Anthony Volk <anth.volk@gmail.com>
2023-11-22 13:26:33 -0600

    test: Add tests for LinkButton

--- src/__tests__/Buttons.test.js
+++ src/__tests__/Buttons.test.js
@@ -5,6 +5,12 @@ import LinkButton from 'controls/LinkButton';
 
 jest.mock("react-plotly.js", () => jest.fn());
 
+const mockUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
+
 describe("Test button types", () => {
   test("Test that Button renders and fires onClick properly", () => {
     const mockOnClick = jest.fn();
@@ -20,10 +26,36 @@ describe("Test button types", () => {
     expect(mockOnClick).toHaveBeenCalledTimes(1);
 
   });
-  test("Test that LinkButton renders properly", () => {
-    return;
+  test("Test that LinkButton renders properly and properly navigates to internal link", () => {
+
+    const testButton = (
+      <LinkButton
+        text="Test Button"
+        link="/test"
+      />
+    );
+
+    const {getByText} = render(testButton);
+
+    fireEvent.click(getByText("Test Button"));
+    expect(mockUseNavigate.mock.calls[0][0]).toBe("/test");
   });
-  test("Test that LinkButton properly navigates to a passed link", () => {
-    return;
+  test("Test that LinkButton properly navigates to external link", () => {
+    window.open = jest.fn();
+
+    const testButton = (
+      <LinkButton
+        text="Test Button"
+        link="https://www.google.com"
+      />
+    );
+
+    const {getByText} = render(testButton);
+
+    fireEvent.click(getByText("Test Button"));
+    expect(window.open).toHaveBeenCalledWith("https://www.google.com", "_blank");
+
+    jest.resetAllMocks();
+
   });
 });
