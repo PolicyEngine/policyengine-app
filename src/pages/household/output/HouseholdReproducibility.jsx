bdcfad04337d5b1eaab76e9c90b44a2dbee161e3
Mauro Busso <mauro.busso12@gmail.com>
2023-11-27 20:06:34 +0000

    removed stale code

--- src/pages/household/output/HouseholdReproducibility.jsx
+++ src/pages/household/output/HouseholdReproducibility.jsx
@@ -172,8 +172,7 @@ export default function HouseholdReproducibility(props) {
         style={{
           display: "flex",
           justifyContent: "center",
-          paddingTop: 30,
-          //marginBottom: 30,
+          paddingTop: 30
         }}
       >
         <Button
