9465815bf1d4198ab81341de25dd5367bd6e9225
Anthony Volk <anth.volk@gmail.com>
2023-11-29 15:09:38 -0600

    feat: Create new form component

---
+++ src/layout/SmallForm.jsx
@@ -0,0 +1,162 @@
+import { useState } from "react";
+import style from "redesign/style";
+import { motion } from "framer-motion";
+
+import Button from "controls/Button";
+
+/**
+ * React component providing a simple form; does not currently handle
+ * radio or select menus, but could be expanded to do so in future
+ * @param {Object} props 
+ * @param {Array<Object>} props.inputFields Each field should be an object inside
+ * this array-type prop and should contain the fields "label", "type" (e.g., "tel"),
+ * "options" (for radio or dropdown), and optional "placeholder"
+ * @param {String} props.submitButtonText="Submit" The text to be placed on
+ * the form submission button; defaults to "Submit"
+ * @param {String} [props.action] The link to 
+ * which the form should submit
+ * @param {String} [props.method] The relevant HTTP method; defaults to "post"
+ * if not provided
+ * @param {Function} [props.onSubmit] An optional onSubmit handler; can
+ * be combined with "action" prop
+ * @param {Function} [props.onClick] An optional click handler for the
+ * form's submit button that overrides merely submitting the form
+ * @param {Object} [props.containerStyle] Style object to style the
+ * form's outer div container
+ * @param {Object} [props.formStyle] Style object for containing form tag
+ * @param {Object} [props.labelStyle] Style object targeted at all
+ * form labels
+ * @param {Object} [props.buttonStyle] Style object for submit button
+ * @param {Object} [props.inputStyle] Style object for all inputs
+ * @param {Boolean} [props.isLocalSubmit] If "true", sends tracked input
+ * object to the optionally provided onSubmit
+ * @returns {import("react").ReactComponentElement}
+ */
+export default function SmallForm(props) {
+  const {
+    action,
+    method,
+    onSubmit,
+    onClick,
+    containerStyle,
+    formStyle,
+    labelStyle,
+    inputStyle,
+    buttonStyle,
+    inputFields,
+    isLocalSubmit,
+    submitButtonText
+  } = props;
+
+  const [formInput, setFormInput] = useState({});
+
+  // Field change handler
+  function handleChange(e) {
+    setFormInput((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value
+    }));
+  }
+
+  // Submission handler only used when onSubmit prop is defined
+  function handleSubmit(e) {
+    if (isLocalSubmit) {
+      (e) => {onSubmit(e, formInput)}
+      onSubmit(formInput);
+    } else {
+      onSubmit(e);
+    }
+  }
+
+  const inputFieldsJSX = inputFields.map((field, index) => (
+    <div 
+      key={index}
+      style={{
+        width: "100%",
+        paddingTop: index > 0 ? "20px" : 0,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        gap: "8px"
+      }}>
+      <label
+        key={index}
+        style={{
+          name: `form-label-${field.label.toLowerCase()}`,
+          fontFamily: "Roboto",
+          fontWeight: 300,
+          color: style.colors.WHITE,
+          textTransform: "uppercase",
+          letterSpacing: 2.4,
+          width: "100%",
+          fontSize: "1rem",
+          lineHeight: "1.2",
+          ...labelStyle
+        }}
+      >
+        {field.label}
+      </label>
+      <motion.input
+        name={field.label}
+        type={field.type || "text"}
+        placeholder={field.placeholder}
+        onChange={handleChange}
+        style={{
+          border: "none",
+          borderBottom: `1px solid ${style.colors.WHITE}`,
+          backgroundColor: style.colors.LIGHT_GRAY,
+          color: style.colors.DARK_GRAY,
+          width: "100%",
+          height: 50,
+          fontSize: 20,
+          fontWeight: 300,
+          padding: 10,
+          boxShadow: `0px 0px 0px ${style.colors.BLUE_PRIMARY}`,
+          ...inputStyle
+        }}
+        // While focussing, make the bottom border blue from left to right
+        whileFocus={{
+          boxShadow: `0px 5px 0px ${style.colors.BLUE_PRIMARY}`,
+        }}
+      />
+    </div>
+    )
+  );
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        ...containerStyle
+      }}
+    >
+      <form
+        onSubmit={onSubmit instanceof Function ? ((e) => handleSubmit(e)) : undefined}
+        action={action}
+        method={method || "post"}
+        style={{
+          width: "100%",
+          ...formStyle
+        }}
+      >
+        {inputFieldsJSX}
+      </form>
+      <Button 
+        text={submitButtonText || "Submit"}
+        onClick={onClick instanceof Function ? ((e) => onClick(e)) : undefined}
+        style={{
+          width: "100%",
+          marginTop: "20px",
+          ...buttonStyle
+        }}
+      />
+    </div>
+  )
+
+
+}
