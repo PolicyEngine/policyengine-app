09d37f717109261fa671be5265a9fe17a75a2437
Anthony Volk <anth.volk@gmail.com>
2023-11-29 15:10:31 -0600

    feat: Use SmallForm for subscriptions, desktop breakpoint

--- src/redesign/components/HomeSubscribe.jsx
+++ src/redesign/components/HomeSubscribe.jsx
@@ -2,6 +2,7 @@ import style from "../style";
 import Section from "./Section";
 import Button from "controls/Button";
 import TextBox from "./TextBox";
+import SmallForm from "../../layout/SmallForm";
 import useDisplayCategory from "./useDisplayCategory";
 
 export default function HomeSubscribe() {
@@ -14,6 +15,17 @@ export default function HomeSubscribe() {
 
 export function SubscribeToPolicyEngine() {
   const displayCategory = useDisplayCategory();
+
+  const submitLink = "https://policyengine.us5.list-manage.com/subscribe/post?u=e5ad35332666289a0f48013c5&amp;id=71ed1f89d8&amp;f_id=00f173e6f0";
+  const submitButtonText = "Subscribe";
+  const inputFields = [
+    {
+      label: "email",
+      type: "email",
+      placeholder: "Enter your email address"
+    }
+  ];
+
   return (
     <div
       style={{
@@ -22,16 +34,34 @@ export function SubscribeToPolicyEngine() {
     >
       {
         {
-          mobile: <SubscribeToPolicyEngineMobile />,
-          tablet: <SubscribeToPolicyEngineTablet />,
-          desktop: <SubscribeToPolicyEngineDesktop />,
+          mobile: <SubscribeToPolicyEngineMobile 
+            inputFields={inputFields}
+            submitLink={submitLink}
+            submitButtonText={submitButtonText}
+          />,
+          tablet: <SubscribeToPolicyEngineTablet 
+            inputFields={inputFields} 
+            submitLink={submitLink}
+            submitButtonText={submitButtonText}
+          />,
+          desktop: <SubscribeToPolicyEngineDesktop 
+            inputFields={inputFields} 
+            submitLink={submitLink}
+            submitButtonText={submitButtonText}
+          />,
         }[displayCategory]
       }
     </div>
   );
 }
 
-function SubscribeToPolicyEngineDesktop() {
+function SubscribeToPolicyEngineDesktop(props) {
+  const {
+    inputFields,
+    submitLink,
+    submitButtonText
+  } = props;
+
   return (
     <div
       style={{
@@ -45,15 +75,15 @@ function SubscribeToPolicyEngineDesktop() {
         <h2 style={{ color: "white" }}>Subscribe to PolicyEngine</h2>
         <p>Get the latests posts delivered right to your inbox.</p>
       </div>
-      <div style={{ width: "40vw" }}>
-        <TextBox
-          title="Email"
-          placeholder="Enter your email address"
-          width={500}
-        />
-        <div style={{ marginTop: 20 }} />
-        <Button text="Subscribe" onClick={() => {}} width={500} />
-      </div>
+      <SmallForm 
+        inputFields={inputFields}
+        action={submitLink}
+        method="post"
+        submitButtonText={submitButtonText}
+        containerStyle={{width: "40vw"}}
+        formStyle={{width: "500px"}}
+        buttonStyle={{width: "500px"}}
+      />
     </div>
   );
 }
