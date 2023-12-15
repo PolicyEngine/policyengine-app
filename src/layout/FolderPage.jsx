4542056a29245273aa02882da740ab3c8f835e13
Anthony Volk <anth.volk@gmail.com>
2023-12-14 23:01:59 -0600

    feat: Make policy selection boxes mobile-friendly grid

--- src/layout/FolderPage.jsx
+++ src/layout/FolderPage.jsx
@@ -101,9 +101,8 @@ export default function FolderPage(props) {
             <motion.div
               key={child.name}
               style={{
-                minWidth: mobile ? 100 : 150,
-                maxWidth: 150,
-                height: mobile ? 100 : 100,
+                width: 150,
+                height: 100,
                 backgroundColor: style.colors.LIGHT_GRAY,
                 margin: 10,
                 padding: 10,
@@ -111,6 +110,7 @@ export default function FolderPage(props) {
                 justifyContent: "center",
                 alignItems: "center",
                 cursor: "pointer",
+                flexGrow: mobile && 1.5
               }}
               whileHover={{
                 scale: 1.05,
