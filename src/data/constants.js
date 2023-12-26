17934fa7795188c5e32e2acd336c6ba80c6d43d3
Anthony Volk <anth.volk@gmail.com>
2023-12-26 15:40:27 -0600

    feat: Add constants representing default year and start & end dates

---
+++ src/data/constants.js
@@ -0,0 +1,3 @@
+export const defaultYear = new Date().getFullYear();
+export const defaultStartDate = defaultYear.concat("-01-01");
+export const defaultEndDate = (defaultYear + 5).concat("-12-31");
