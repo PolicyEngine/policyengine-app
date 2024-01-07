4d7fe7d85297070bde087973c7e93fe01d3e9a22
Anthony Volk <anth.volk@gmail.com>
2024-01-06 22:44:11 -0600

    feat: Initial template for HouseholdYear

---
+++ src/pages/household/input/HouseholdYear.jsx
@@ -0,0 +1,10 @@
+import CenteredMiddleColumn from "layout/CenteredMiddleColumn";
+
+
+export default function HouseholdYear(props) {
+  return (
+    <CenteredMiddleColumn
+      title="Which tax year would you like to calculate?"
+    />
+  );
+}
