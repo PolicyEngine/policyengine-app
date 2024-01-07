ab7c5d2156a609bab9fa4d1df2997b65154048cf
Anthony Volk <anth.volk@gmail.com>
2024-01-06 20:44:11 -0600

    fix: Reset InputField test state between runs

--- src/__tests__/InputField.test.js
+++ src/__tests__/InputField.test.js
@@ -2,10 +2,6 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import InputField from "../controls/InputField";
 import { useSearchParams } from "react-router-dom";
 
-const testProps = {
-  placeholder: "Test Placeholder",
-  onChange: jest.fn(),
-};
 jest.mock("react-router-dom", () => {
   const originalModule = jest.requireActual("react-router-dom");
   return {
@@ -17,18 +13,30 @@ jest.mock("react-router-dom", () => {
 const patternProp = { pattern: "%" };
 const testInput = "Test Input";
 
-const setup = (props) => {
-  const utils = render(<InputField {...props} />);
-  const input = screen.getByPlaceholderText(testProps.placeholder);
-
-  return {
-    input,
-    ...utils,
-  };
-};
-
 describe("Should take inputs", () => {
+  let testProps = null;
+  let setup = null;
+
+  beforeEach(() => {
+    testProps = {
+      placeholder: "Test Placeholder",
+      onChange: jest.fn(),
+    };
+
+    setup = (props) => {
+      const utils = render(<InputField {...props} />);
+      const input = screen.getByPlaceholderText(testProps.placeholder);
+    
+      return {
+        input,
+        ...utils,
+      };
+    };
+
+  });
+
   test("Should handle non-percent input & submit on blur", () => {
+    console.log(testProps.onChange.mock);
     useSearchParams.mockImplementation(() => {
       const get = () => "gov.irs.ald.loss.capital.max.HEAD_OF_HOUSEHOLD";
       return [{ get }];
@@ -37,12 +45,15 @@ describe("Should take inputs", () => {
     fireEvent.change(input, { target: { value: testInput } });
     expect(input.value).toBe(testInput);
     expect(testProps.onChange).not.toHaveBeenCalled();
+    console.log(testProps.onChange.mock);
 
     fireEvent.blur(input);
     expect(testProps.onChange).toHaveBeenCalledWith(testInput);
+    console.log(testProps.onChange.mock);
   });
 
   test("Should append '%' for percent pattern & submit on blur", () => {
+    console.log(testProps.onChange.mock);
     useSearchParams.mockImplementation(() => {
       const get = () => "gov.irs.ald.loss.capital.max.HEAD_OF_HOUSEHOLD";
       return [{ get }];
@@ -51,8 +62,10 @@ describe("Should take inputs", () => {
     fireEvent.change(input, { target: { value: testInput } });
     expect(input.value).toBe(testInput + "%");
     expect(testProps.onChange).not.toHaveBeenCalled();
+    console.log(testProps.onChange.mock);
 
     fireEvent.blur(input);
     expect(testProps.onChange).toHaveBeenCalledWith(testInput + "%");
+    console.log(testProps.onChange.mock);
   });
 });
