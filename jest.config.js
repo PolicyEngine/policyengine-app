8122431095c97e714bdd22f688a513cdbb82ff63
Anthony Volk <anth.volk@gmail.com>
2024-01-06 19:58:34 -0600

    fix: Add import mocks for images and CSS

--- jest.config.js
+++ jest.config.js
@@ -21,5 +21,9 @@ module.exports = {
   ],
   testMatch: [
     "**/__tests__/**/*.test.(ts|js)"
-  ]
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__tests__/setup/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/src/__tests__/setup/fileMock.js"
+  }
 }
---
+++ src/__tests__/setup/fileMock.js
@@ -0,0 +1,3 @@
+// Directions for setting up this mock file are taken from
+// https://stackoverflow.com/questions/46898638/importing-images-breaks-jest-test
+export default ' ';
