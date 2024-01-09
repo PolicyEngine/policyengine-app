e82c0c36289d90d825be16b24a9fb99cdc3b0b8b
Jigin Jayaprakash <145117767+JiginJayaprakash@users.noreply.github.com>
2024-01-09 12:43:35 +0000

    1056: reform data remove if empty object

--- src/pages/policy/PolicyRightSidebar.jsx
+++ src/pages/policy/PolicyRightSidebar.jsx
@@ -199,6 +199,9 @@ function PolicyItem(props) {
 function PolicyDisplay(props) {
   const { policy, metadata, region, timePeriod, closeDrawer, hideButtons } =
     props;
+  policy.reform.data = Object.fromEntries(
+    Object.entries(policy.reform.data).filter(([key, value]) => !(Object.keys(value).length === 0 && value.constructor === Object))
+  );
   const reformLength = Object.keys(policy.reform.data).length;
   const [searchParams, setSearchParams] = useSearchParams();
   return (
