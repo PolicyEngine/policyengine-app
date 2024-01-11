bcfc6221661a22ad0eeeb292e52e1cafaa9aed9f
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-11 11:13:59 +0530

    running lint

--- src/layout/FolderPage.jsx
+++ src/layout/FolderPage.jsx
@@ -1,7 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import { copySearchParams } from "../api/call";
 import style from "../style";
-
 import ResultsPanel from "./ResultsPanel";
 import { motion } from "framer-motion";
 import useMobile from "./Responsive";
