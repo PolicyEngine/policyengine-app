a840c62222521e68185cce170d8c1a192674d479
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-24 00:26:09 +0530

    updated link color

--- src/pages/policy/output/PolicyReproducibility.jsx
+++ src/pages/policy/output/PolicyReproducibility.jsx
@@ -31,7 +31,7 @@ export default function PolicyReproducibility(props) {
       href={colabLink}
       target="_blank"
       rel="noreferrer"
-      style={{ color: colors.TEAL_ACCENT, textDecoration: "underline" }}
+      style={{ color: colors.BLUE_PRIMARY, textDecoration: "underline" }}
     >
       Python notebook
     </a>
