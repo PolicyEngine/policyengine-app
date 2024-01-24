ff864d6b4bc3af2ee0f0bbe1bc92c793a8a45969
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-24 20:20:58 +0530

    how to donate section link color is now same as its heading

--- src/redesign/components/Donate.jsx
+++ src/redesign/components/Donate.jsx
@@ -90,7 +90,10 @@ export default function Donate() {
                 We accept credit card, bank transfer, or check. After
                 contributing, please email{" "}
                 <u>
-                  <a href="mailto:hello@policyengine.org">
+                  <a
+                    href="mailto:hello@policyengine.org"
+                    style={{ color: "inherit" }}
+                  >
                     hello@policyengine.org
                   </a>
                 </u>{" "}
