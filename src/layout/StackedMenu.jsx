09d80006c616702a4d94a26b1747004ab07b7dff
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-24 20:48:20 +0530

    now after every variable change the scroller will be at the top

--- src/layout/StackedMenu.jsx
+++ src/layout/StackedMenu.jsx
@@ -1,16 +1,34 @@
+import { useEffect, useRef } from "react";
 import Divider from "./Divider";
 import Menu from "./Menu";
 
 export default function StackedMenu(props) {
   const { firstTree, secondTree, selected, onSelect } = props;
+  const firstMenuRef = useRef(null);
+  const secondMenuRef = useRef(null);
+
+  useEffect(() => {
+    if (firstMenuRef.current) {
+      firstMenuRef.current.scrollTop = 0;
+    }
+    if (secondMenuRef.current) {
+      secondMenuRef.current.scrollTop = 0;
+    }
+  }, [selected]);
 
   return (
     <div style={{ height: "80vh" }}>
-      <div style={{ overflow: "scroll", height: "50%", padding: 20 }}>
+      <div
+        ref={firstMenuRef}
+        style={{ overflow: "scroll", height: "50%", padding: 20 }}
+      >
         <Menu tree={firstTree} selected={selected} onSelect={onSelect} />
       </div>
       <Divider />
-      <div style={{ overflow: "scroll", height: "50%", padding: 20 }}>
+      <div
+        ref={secondMenuRef}
+        style={{ overflow: "scroll", height: "50%", padding: 20 }}
+      >
         <Menu tree={secondTree} selected={selected} onSelect={onSelect} />
       </div>
     </div>
