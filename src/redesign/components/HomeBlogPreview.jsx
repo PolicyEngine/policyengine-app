671684a9c259bfc24e64d0a72420973935e3d0d6
Abhishek Singh <31144719+abhcs@users.noreply.github.com>
2024-01-26 22:19:44 -0600

    fix: revert commit 4fa4ad9 to fix blog.image error

--- src/redesign/components/HomeBlogPreview.jsx
+++ src/redesign/components/HomeBlogPreview.jsx
@@ -17,9 +17,7 @@ export default function HomeBlogPreview() {
         (post.tags.includes(countryId) || post.tags.includes("global")),
     ) || [];
   const allPosts = posts.filter(
-    (post) =>
-      (post.tags.includes(countryId) || post.tags.includes("global")) &&
-      !featuredPosts.includes(post),
+    (post) => post.tags.includes(countryId) || post.tags.includes("global"),
   );
   const displayCategory = useDisplayCategory();
   return (
