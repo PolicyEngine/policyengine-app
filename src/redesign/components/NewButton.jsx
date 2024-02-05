b0bd94de66c8781bc8015adf88356b483acdaa6a
Sahil Mahapatra <sahilm231199@gmail.com>
2024-02-06 03:49:11 +0530

    renoved css hover effects and used onMouse events

--- src/redesign/components/NewButton.jsx
+++ src/redesign/components/NewButton.jsx
@@ -2,6 +2,25 @@ import React from "react";
 import { Button } from "antd";
 import colors from "../style/colors";
 
+const buttonStyles = {
+  primary: {
+    hoverBackgroundColor: colors.TEAL_PRESSED,
+    standardBackgroundColor: colors.TEAL_ACCENT,
+  },
+  secondary: {
+    hoverBackgroundColor: colors.BLUE_PRESSED,
+    standardBackgroundColor: colors.BLUE_LIGHT,
+  },
+  disabled: {
+    hoverBackgroundColor: colors.BLUE_PRESSED,
+    standardBackgroundColor: colors.BLUE_LIGHT,
+  },
+  default: {
+    hoverBackgroundColor: colors.TEAL_PRESSED,
+    standardBackgroundColor: colors.TEAL_ACCENT,
+  },
+};
+
 const NewButton = (props) => {
   return (
     <Button
@@ -11,6 +30,14 @@ const NewButton = (props) => {
         borderRadius: "0",
         backgroundColor: colors.TEAL_ACCENT,
       }}
+      onMouseOver={(e) =>
+        (e.currentTarget.style.backgroundColor = colors.TEAL_PRESSED) &&
+        (e.currentTarget.style.borderColor = colors.TEAL_PRESSED)
+      }
+      onMouseOut={(e) =>
+        (e.currentTarget.style.backgroundColor = colors.TEAL_ACCENT) &&
+        (e.currentTarget.style.borderColor = colors.TEAL_ACCENT)
+      }
     >
       🎉New Button
     </Button>
