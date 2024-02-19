3aab1332903a21ade2a81e90371c0419c86667bc
Anthony V <anth.volk@gmail.com>
2024-02-19 18:26:16 +0200

    Properly fetch renamed policy (#1367)
    * fix: Properly change reform number when name changes
    
    * feat: Add feature to test if policy objects are the same, as well as tests
    
    * feat: Check if policy has changed before enqueuing calculation in output pages
    
    * fix: Remove dead logs
    
    * chore: Lint

---
+++ src/__tests__/pages/policy/output/FetchAndDisplayImpact.test.js
@@ -0,0 +1,116 @@
+import { areObjectsSame } from "../../../../pages/policy/output/FetchAndDisplayImpact";
+
+jest.mock("react-plotly.js", () => jest.fn());
+
+describe("areObjectsSame function", () => {
+  const testObj = {
+    firstKey: "maxwell",
+    secondKey: "dworkin",
+    thirdKey: "cruft",
+  };
+
+  test("First object is null should return false", () => {
+    const firstObj = null;
+    const secondObj = JSON.parse(JSON.stringify(testObj));
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+  test("Second object is null should return false", () => {
+    const firstObj = JSON.parse(JSON.stringify(testObj));
+    const secondObj = null;
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+  test("Different key lengths should return false", () => {
+    const firstObj = JSON.parse(JSON.stringify(testObj));
+    const secondObj = {
+      firstKey: "garbageValue",
+    };
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+  test("Same number of keys, but different keys, should return false", () => {
+    const firstObj = JSON.parse(JSON.stringify(testObj));
+    const secondObj = {
+      firstKey: "garbageValue",
+      secondKey: "northwestern",
+      invalidKey: "invalidValue",
+    };
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+  test("Different values should return false", () => {
+    const firstObj = JSON.parse(JSON.stringify(testObj));
+    const secondObj = {
+      firstKey: "garbageValue",
+      secondKey: "northwestern",
+      thirdKey: "invalidValue",
+    };
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+  test("Exact same keys and values should return true", () => {
+    const firstObj = JSON.parse(JSON.stringify(testObj));
+    const secondObj = {
+      firstKey: "maxwell",
+      secondKey: "dworkin",
+      thirdKey: "cruft",
+    };
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(true);
+  });
+  test("Exact same nested objects should return true", () => {
+    const nestedTestObj = {
+      firstKey: "maxwell",
+      secondKey: "dworkin",
+      nestedKey: {
+        firstSubKey: "maxwell",
+        secondSubKey: "cruft",
+      },
+    };
+
+    const firstObj = JSON.parse(JSON.stringify(nestedTestObj));
+    const secondObj = JSON.parse(JSON.stringify(nestedTestObj));
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(true);
+  });
+  test("One nested object and one not should return false", () => {
+    const nestedTestObj = {
+      firstKey: "maxwell",
+      secondKey: "dworkin",
+      nestedKey: {
+        firstSubKey: "maxwell",
+        secondSubKey: "cruft",
+      },
+    };
+
+    const firstObj = JSON.parse(JSON.stringify(testObj));
+    const secondObj = JSON.parse(JSON.stringify(nestedTestObj));
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+  test("Different nested objects should return false", () => {
+    const nestedTestObj = {
+      firstKey: "maxwell",
+      secondKey: "dworkin",
+      nestedKey: {
+        firstSubKey: "maxwell",
+        secondSubKey: "cruft",
+      },
+    };
+
+    const otherTestObj = {
+      firstKey: "maxwell",
+      secondKey: "garbageValue",
+      nestedKey: {
+        firstSubKey: "maxwell",
+        secondSubKey: "illegalValue",
+      },
+    };
+
+    const firstObj = JSON.parse(JSON.stringify(otherTestObj));
+    const secondObj = JSON.parse(JSON.stringify(nestedTestObj));
+
+    expect(areObjectsSame(firstObj, secondObj)).toEqual(false);
+  });
+});
--- src/pages/policy/output/FetchAndDisplayImpact.jsx
+++ src/pages/policy/output/FetchAndDisplayImpact.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   DisplayError,
   DisplayImpact,
@@ -43,7 +43,16 @@ export function FetchAndDisplayImpact(props) {
     hasShownPopulationImpactPopup,
     setHasShownPopulationImpactPopup,
   } = props;
+  const policyRef = useRef(null);
+
   useEffect(() => {
+    if (
+      areObjectsSame(policy?.reform?.data, policyRef.current?.reform?.data) &&
+      areObjectsSame(policy?.baseline?.data, policyRef.current?.baseline?.data)
+    ) {
+      return;
+    }
+
     if (!!region && !!timePeriod && !!reformPolicyId && !!baselinePolicyId) {
       const selectedVersion = searchParams.get("version") || metadata.version;
       const url = `/${metadata.countryId}/economy/${reformPolicyId}/over/${baselinePolicyId}?region=${region}&time_period=${timePeriod}&version=${selectedVersion}`;
@@ -121,6 +130,7 @@ export function FetchAndDisplayImpact(props) {
       );
       setSearchParams(newSearch);
     }
+    policyRef.current = policy;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [region, timePeriod, reformPolicyId, baselinePolicyId]);
 
@@ -254,4 +264,52 @@ export function FetchAndDisplayCliffImpact(props) {
     />
   );
   */
+}
+
+/**
+ * Function to recursively compare two objects
+ * @param {null | Object} firstObject
+ * @param {null | Object} secondObject
+ * @returns {Boolean} Whether or not the two objects are
+ * the same
+ */
+export function areObjectsSame(firstObject, secondObject) {
+  // Ensure that both objects fed to the function are,
+  // in fact, objects; return false if one isn't
+  if (firstObject === null || typeof firstObject !== "object") {
+    return false;
+  }
+
+  if (secondObject === null || typeof secondObject !== "object") {
+    return false;
+  }
+
+  // Take the keys of both
+  const firstObjKeys = Object.keys(firstObject);
+  const secondObjKeys = Object.keys(secondObject);
+
+  // Return false if the key arrays aren't the same length
+  if (firstObjKeys.length !== secondObjKeys.length) {
+    return false;
+  }
+
+  // For each key in firstObjKeys...
+  for (const key of firstObjKeys) {
+    // Access the two objects at said key
+    const firstVal = firstObject[key];
+    const secondVal = secondObject[key] || null;
+
+    // If both values are objects, recurse and return
+    if (typeof firstVal === "object" && typeof secondVal === "object") {
+      return areObjectsSame(firstVal, secondVal);
+    }
+
+    // If the values aren't the same, return false
+    if (firstVal !== secondVal) {
+      return false;
+    }
+  }
+
+  // If we made it this far, return true
+  return true;
 }
