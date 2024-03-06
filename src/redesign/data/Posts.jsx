6bd2ce249ad5752b50ba6c37982b10d379813bc4
Anthony V <anth.volk@gmail.com>
2024-03-06 11:30:50 +0200

    Spring Budget 2024 post (#1444)
    * feat: First version of Spring Budget 2024
    
    * feat: NIC cuts, macro changes; NUMBERS ARE OLD AND SHOULD BE CHANGED WHEN API UPDATES
    
    * feat: NIC cuts, micro changes; NUMBERS ARE OLD AND MUST BE UPDATED WHEN API UPDATES
    
    * feat: update values based on API updates
    
    * feat: Updates to article
    
    * Update post
    
    Co-authored-by: Anthony V. <anth-volk@users.noreply.github.com>
    
    * Edit text
    
    * fix: Bring formatting into line with expectations
    
    * feat: Rewritten 2p decrease to National Insurance section
    
    * feat: Section on 1p National Insurance decrease
    
    * feat: Section about Basic Rate
    
    * feat: Add further data
    
    * fix: Remove fuel duty
    
    * feat: Conclusion
    
    * feat: Description of distributional impacts chart
    
    * chore: Lint
    
    * Add anthony authorship
    
    ---------
    
    Co-authored-by: Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
    Co-authored-by: Anthony V. <anth-volk@users.noreply.github.com>

--- src/redesign/data/Posts.jsx
+++ src/redesign/data/Posts.jsx
@@ -735,6 +735,16 @@ let posts = [
     image: "wyden.jpeg",
     authors: ["max-ghenis"],
   },
+  {
+    title: "Spring Budget 2024 proposals",
+    description:
+      "PolicyEngine estimates reported likely tax-benefit reforms ahead of the UK Spring Budget.",
+    date: "2024-03-04 09:00:00",
+    filename: "spring-budget-2024.ipynb",
+    tags: ["uk", "policy", "featured"],
+    image: "jeremy_hunt.jpg",
+    authors: ["anthony-volk", "nikhil-woodruff"],
+  },
 ].sort((a, b) => (a.date < b.date ? 1 : -1));
 
 for (let post of posts) {
