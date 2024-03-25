dd1062e71eb6c065f8486fa3bb1e0efe3bf60fc1
Anthony Volk <anth.volk@gmail.com>
2024-03-26 01:00:56 +0200

    chore: Refactor Auth0ProviderWithNavigate

---
+++ src/contexts/Auth0ProviderWithNavigate.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+export default function Auth0ProviderWithNavigate({children}) {
+  const navigate = useNavigate();
+
+  const redirectUri = "http://localhost:3000/callback";
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  };
+
+  if (!(domain && clientId && redirectUri)) {
+    return null;
+  }
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+}
