86ed7a3d1cdc0fa0801ff7c3314415f7170f39c7
Anthony Volk <anth.volk@gmail.com>
2024-03-27 16:54:57 +0200

    feat: First version of signup modal

---
+++ src/modals/SignupModal.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from "react";
+import { Modal } from "antd";
+import FormContext from "../layout/forms/FormContext";
+import FormItem from "../layout/forms/FormItem";
+
+export default function SignupModal() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  return (
+    <Modal 
+      open={isModalOpen}
+      footer={null}
+    >
+      <h6
+        style={{
+          paddingBottom: "24px",
+          paddingTop: "20px",
+          fontWeight: "bold",
+          fontSize: 20
+        }}
+      >PolicyEngine depends on you</h6>
+      <p>PolicyEngine&apos;s free, open-source software relies on your feedback to make further improvements.</p>
+      <p>To see your results, please provide your email address below.</p>
+      <FormContext
+        submitButtonText="Submit"
+      >
+        <FormItem
+          label="Email address"
+          type="email"
+        />
+      </FormContext>
+    </Modal>
+  );
+}
