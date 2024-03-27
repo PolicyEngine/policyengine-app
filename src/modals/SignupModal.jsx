840fde4741cdc451e1ad2304adecc1678b2a1352
Anthony Volk <anth.volk@gmail.com>
2024-03-27 20:38:18 +0200

    feat: Hash input password, though unclear what to do with for time being

--- src/modals/SignupModal.jsx
+++ src/modals/SignupModal.jsx
@@ -4,16 +4,32 @@ import { Modal } from "antd";
 import FormContext from "../layout/forms/FormContext";
 import FormItem from "../layout/forms/FormItem";
 import colors from "../style/colors";
+import bcrypt from "bcryptjs";
 
 export default function SignupModal() {
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [submitMsg, setSubmitMsg] = useState("");
 
-  function handleSubmit(event, formInput) {
+  function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    const fullCookie = cookies.find((cookie) => cookie.trim().startsWith(`${name}=`));
+    return fullCookie.split("=")[1];
+  }
+
+  /**
+   * Cookie setter function
+   * @param {String} name 
+   * @param {String} value 
+   * @param {Number|String} [maxAge=31536000] The maximum cookie age, in ms
+   * @param {String} [path="/"] The cookie path
+   */
+  function setCookie(name, value, maxAge=31536000, path) {
+    document.cookie = `${name}=${value};max-age=${String(maxAge)};path=${path}`;
+  }
+
+  async function handleSubmit(event, formInput) {
     // Prevent immediate submission
     event.preventDefault();
-    console.log(formInput);
-    console.log(event);
 
     // Validate that email is valid
     // Regex taken from https://stackabuse.com/validate-email-addresses-with-regular-expressions-in-javascript/
@@ -23,11 +39,31 @@ export default function SignupModal() {
       setSubmitMsg("Error: Invalid email provided; please try again");
     }
 
+    // If user hasn't consented to storing cookies, indicate
+    // that they must
+    if (!getCookie("consent")) {
+      setSubmitMsg("This operation requires cookies to be enabled");
+      return;
+    }
+
     // Store cookie with hash of email
+    // For the time being, this will just log to console,
+    // since it's unclear how this should be implemented
+    const salt = await bcrypt.genSalt(10);
+    const emailHash = await bcrypt.hash(formInput.email, salt);
+    console.debug(emailHash);
+
+    /*
+    setCookie(
+      "email_hash",
+      hashValue,
+    );
+    */
 
     // Don't need to POST it anywhere - this should be 
     // done by the policy calculation component, which should
-    // check for cookie presence, then POST if present
+    // check for cookie presence, then POST if present;
+    // or should we POST it somewhere? Unclear at moment
 
     // Check if user wants to sign up for newsletter
       // Don't do anything if box isn't checked
