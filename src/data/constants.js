86370b6e409bbab017e5d8e69c6ca0fb7506bcec
Anthony Volk <anth.volk@gmail.com>
2024-03-28 22:59:04 +0200

    feat: Replace default end date with a 'forever' value of 2100-12-31

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,9 @@
+// The below is used to make parameter editor components appear as
+// if a reform is applied into perpetuity; this should be replaced
+// with more substantive changes to how parameter changes work to 
+// allow for application into perpetuity, if that is desired behavior
+export const defaultForever = "2100"
+
 export const defaultYear = new Date().getFullYear();
 export const defaultStartDate = defaultYear.toString().concat("-01-01");
-export const defaultEndDate = defaultYear.toString().concat("-12-31");
+export const defaultEndDate = defaultForever.toString().concat("-12-31");
