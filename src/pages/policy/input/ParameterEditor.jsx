4873c531f52b6092f4a6b7d3d8c4095b64b59101
Anthony Volk <anth.volk@gmail.com>
2024-04-03 15:46:12 +0200

    chore: Refactor ParameterEditor, breaking out ValueSetter and PeriodSetter

--- src/pages/policy/input/ParameterEditor.jsx
+++ src/pages/policy/input/ParameterEditor.jsx
@@ -1,11 +1,10 @@
 import CenteredMiddleColumn from "../../../layout/CenteredMiddleColumn";
 import ParameterOverTime from "./ParameterOverTime";
-import { Alert, Button, DatePicker, Popover, Segmented, Switch, Tabs, Tooltip } from "antd";
+import { Alert, Button, DatePicker, Popover, Segmented, Switch, Tooltip } from "antd";
 import { getNewPolicyId } from "../../../api/parameters";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { copySearchParams } from "../../../api/call";
-import useMobile from "../../../layout/Responsive";
 import { capitalize, localeCode } from "../../../lang/format";
 import { currencyMap } from "../../../api/variables";
 import { defaultStartDate, defaultEndDate, defaultForeverYear } from "data/constants";
@@ -17,6 +16,15 @@ import useDisplayCategory from "redesign/components/useDisplayCategory";
 
 const { RangePicker } = DatePicker;
 
+/**
+ * Component used to edit the values for a parameter; this is itself
+ * composed of two sub-components: PeriodSetter and ValueSetter
+ * @param {Object} props 
+ * @param {Object} props.metadata
+ * @param {Object} props.policy
+ * @param {String} props.parameterName
+ * @returns {import("react-markdown/lib/react-markdown").ReactElement}
+ */
 export default function ParameterEditor(props) {
   const { metadata, policy, parameterName } = props;
   const parameter = metadata.parameters[parameterName];
@@ -31,13 +39,10 @@ export default function ParameterEditor(props) {
     }
   }
 
-  const [searchParams, setSearchParams] = useSearchParams();
   const [startDate, setStartDate] = useState(defaultStartDate);
   const [endDate, setEndDate] = useState(defaultEndDate);
-  const [visibleDateSelector, setVisibleDateSelector] = useState("yearly");
 
   const displayCategory = useDisplayCategory();
-  const startValue = reformMap.get(startDate);
 
   useEffect(() => {
 
@@ -54,101 +59,117 @@ export default function ParameterEditor(props) {
     }
   }, [reformData]);
 
-  function handleSegmentedChange(value) {
-    setVisibleDateSelector(value);
-  }
-
-  function onChange(value) {
-    reformMap.set(startDate, nextDay(endDate), value);
-    let data = {};
-    reformMap.minus(baseMap).forEach(([k1, k2, v]) => {
-      data[`${k1}.${prevDay(k2)}`] = v;
-    });
-    const newReforms = { ...policy.reform.data };
-    if (
-      Object.keys(data).length === 0 &&
-      Object.keys(newReforms).length === 1
-    ) {
-      let newSearch = copySearchParams(searchParams);
-      newSearch.delete("reform");
-      setSearchParams(newSearch);
-    } else {
-      newReforms[parameterName] = data;
-      getNewPolicyId(metadata.countryId, newReforms).then((result) => {
-        if (result.status !== "ok") {
-          console.error(
-            "ParameterEditor: In attempting to fetch new " +
-              "policy, the following error occurred: " +
-              result.message,
-          );
-        } else {
-          let newSearch = copySearchParams(searchParams);
-          newSearch.set("reform", result.policy_id);
-          setSearchParams(newSearch);
-        }
-      });
-    }
-  }
-
-  let control;
-
-  if (parameter.unit === "bool" || parameter.unit === "abolition") {
-    control = (
-      <div style={{ padding: 10 }}>
-        <Switch
-          key={"input for" + parameter.parameter}
-          defaultChecked={startValue}
-          onChange={(value) => onChange(!!value)}
-        />
-      </div>
-    );
-  } else {
-    const isPercent = parameter.unit === "/1";
-    const scale = isPercent ? 100 : 1;
-    const isCurrency = Object.keys(currencyMap).includes(parameter.unit);
-    const maximumFractionDigits = 2;
-    control = (
-      <StableInputNumber
-        style={{
-          width: displayCategory === "mobile" ? "50%" : "100%",
-        }}
-        key={"input for" + parameter.parameter}
-        {...(isCurrency
-          ? {
-              addonBefore: currencyMap[parameter.unit],
-            }
-          : {})}
-        {...(isPercent
-          ? {
-              addonAfter: "%",
-            }
-          : {})}
-        formatter={(value, { userTyping }) => {
-          const n = +value;
-          const isInteger = Number.isInteger(n);
-          return n.toLocaleString(localeCode(metadata.countryId), {
-            minimumFractionDigits: userTyping || isInteger ? 0 : 2,
-            maximumFractionDigits: userTyping ? 16 : maximumFractionDigits,
-          });
-        }}
-        defaultValue={Number(startValue) * scale}
-        onPressEnter={(_, value) =>
-          onChange(+value.toFixed(maximumFractionDigits) / scale)
-        }
-      />
-    );
-  }
-
   useEffect(() => {
     console.log(startDate);
     console.log(endDate + "\n");
   }, [startDate, endDate])
+
+  let gridTemplate = null;
+  if (displayCategory === "mobile" || displayCategory === "tablet") {
+    gridTemplate = "repeat(2, 1fr) / 1fr";
+  } else {
+    gridTemplate = "1fr / repeat(2, 1fr)";
+  }
+
+  const timePeriodSentence = parameter.period
+    ? ` This parameter is ${parameter.period}ly.`
+    : "";
+
+  let description = parameter.description;
+  if (!description) {
+    description = "";
+  }
+
+  return (
+    <CenteredMiddleColumn
+      marginTop="5%"
+      marginBottom={0}
+      title={capitalize(parameter.label)}
+      description={description + timePeriodSentence}
+    >
+      <div
+        style={{
+          display: "grid",
+          justifyItems: "center",
+          gridTemplate: gridTemplate,
+          alignItems: "center",
+          padding: displayCategory !== "mobile" && "20px 80px 0 80px",
+          paddingTop: 20,
+          gap: 10,
+          width: "100%",
+        }}
+      >
+        <PeriodSetter 
+          metadata={metadata}
+          startDate={startDate}
+          endDate={endDate}
+          setStartDate={setStartDate}
+          setEndDate={setEndDate}
+        />
+        <ValueSetter 
+          startDate={startDate}
+          endDate={endDate}
+          parameterName={parameterName}
+          policy={policy}
+          metadata={metadata}
+          reformMap={reformMap}
+          baseMap={baseMap}
+        />
+      </div>
+      {!parameter.economy && (
+        <Alert
+          message="PolicyEngine does not currently model this parameter in society-wide economic simulations."
+          type="warning"
+        />
+      )}
+      <ParameterOverTime
+        baseMap={baseMap}
+        {...(reformData &&
+          Object.keys(reformData).length > 0 && {
+            reformMap: reformMap,
+          })}
+        parameter={parameter}
+        policy={policy}
+        metadata={metadata}
+      />
+    </CenteredMiddleColumn>
+  );
+}
+
+function PeriodSetter(props) {
+  const {
+    metadata,
+    startDate,
+    endDate,
+    setStartDate,
+    setEndDate
+  } = props;
+
+  const [visibleDateSelector, setVisibleDateSelector] = useState("yearly");
+
+  function handleSegmentedChange(value) {
+    setVisibleDateSelector(value);
+  }
 
   // Array of selectable years, sorted in ascending order
   const possibleYears = metadata.economy_options.time_period.map((period) => period.name).sort();
   const minPossibleDate = String(possibleYears[0]).concat("-01-01");
   const maxPossibleDate = String(possibleYears[possibleYears.length - 1] + 10).concat("-12-31");
   const defaultEnd = endDate === String(defaultForeverYear).concat("-12-31") ? null : moment(endDate);
+
+  let dateSelectButtonLabel = "";
+  const isFullYearSet = (
+    checkBoundaryDate(startDate, "start") &&
+    checkBoundaryDate(endDate, "end")
+  );
+
+  if (!isFullYearSet) {
+    dateSelectButtonLabel = `from ${moment(startDate).format("MMMM Do, YYYY")} to ${moment(endDate).format("MMMM Do, YYYY")}:`;
+  } else if (moment(endDate).year() === Number(defaultForeverYear)) {
+    dateSelectButtonLabel = `from ${moment(startDate).year()} onward:`;
+  } else {
+    dateSelectButtonLabel = `from ${moment(startDate).year()} to ${moment(endDate).year()}:`;
+  }
 
   const yearSelector = (
     <RangePicker
@@ -210,121 +231,120 @@ export default function ParameterEditor(props) {
     </div>
   );
 
-  let dateSelectButtonLabel = "";
-  const isFullYearSet = (
-    checkBoundaryDate(startDate, "start") &&
-    checkBoundaryDate(endDate, "end")
-  );
-
-  if (!isFullYearSet) {
-    dateSelectButtonLabel = `from ${moment(startDate).format("MMMM Do, YYYY")} to ${moment(endDate).format("MMMM Do, YYYY")}:`;
-  } else if (moment(endDate).year() === Number(defaultForeverYear)) {
-    dateSelectButtonLabel = `from ${moment(startDate).year()} onward:`;
-  } else {
-    dateSelectButtonLabel = `from ${moment(startDate).year()} to ${moment(endDate).year()}:`;
-  }
-
-  let gridTemplate = null;
-  if (displayCategory === "mobile" || displayCategory === "tablet") {
-    gridTemplate = "repeat(2, 1fr) / 1fr";
-  } else {
-    gridTemplate = "1fr / repeat(2, 1fr)";
-  }
-
-  const editControl = (
-    <div
-      style={{
-        display: "grid",
-        justifyItems: "center",
-        gridTemplate: gridTemplate,
-        alignItems: "center",
-        padding: displayCategory !== "mobile" && "20px 80px 0 80px",
-        paddingTop: 20,
-        gap: 10,
-        width: "100%",
-        fontFamily: "Roboto Serif",
-      }}
-    >
-      <Popover
-        trigger="click"
-        content={popoverContent}
-        placement="bottom"
-      >
-        <Tooltip title="Click to edit parameter timespan">
-          <Button 
-            type="text"
-            style={{
-              width: "100%"
-            }}
-          >
-            {dateSelectButtonLabel}
-          </Button>
-        </Tooltip>
-      </Popover>
-      {control}
-    </div>
-  );
-
-  let description = parameter.description;
-  if (!description) {
-    description = "";
-  }
-
   return (
-    <CenteredMiddleColumn
-      marginTop="5%"
-      marginBottom={0}
-      title={capitalize(parameter.label)}
-      description={description}
+    <Popover
+      trigger="click"
+      content={popoverContent}
+      placement="bottom"
     >
-      <div>
-        <p
+      <Tooltip title="Click to edit parameter timespan">
+        <Button 
+          type="text"
           style={{
-            marginBottom: 2,
-            fontFamily: "Roboto",
-            marginTop: 10,
-            color: "grey",
-            display: "inline-block",
+            width: "100%"
           }}
         >
-          Current value
-        </p>
-      </div>
-      {editControl}
-      {!parameter.economy && (
-        <div style={{ paddingTop: 20 }}>
-          <Alert
-            message="PolicyEngine does not currently model this parameter in society-wide economic simulations."
-            type="warning"
-          />
-        </div>
-      )}
-      <div style={{ paddingRight: 30, marginTop: 30 }}>
-        <p
-          style={{
-            marginBottom: 0,
-            color: "grey",
-            display: "inline-block",
-            fontFamily: "Roboto",
-          }}
-        >
-          Historical values
-        </p>
-      </div>
-      <div style={{ marginLeft: -25 }}>
-        <ParameterOverTime
-          baseMap={baseMap}
-          {...(reformData &&
-            Object.keys(reformData).length > 0 && {
-              reformMap: reformMap,
-            })}
-          parameter={parameter}
-          policy={policy}
-          metadata={metadata}
+          {dateSelectButtonLabel}
+        </Button>
+      </Tooltip>
+    </Popover>
+  );
+}
+
+function ValueSetter(props) {
+  const {
+    startDate,
+    endDate,
+    parameterName,
+    metadata,
+    reformMap,
+    baseMap,
+    policy
+  } = props;
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const displayCategory = useDisplayCategory();
+  const startValue = reformMap.get(startDate);
+  const parameter = metadata.parameters[parameterName];
+
+  function changeHandler(value) {
+    reformMap.set(startDate, nextDay(endDate), value);
+    let data = {};
+    reformMap.minus(baseMap).forEach(([k1, k2, v]) => {
+      data[`${k1}.${prevDay(k2)}`] = v;
+    });
+    const newReforms = { ...policy.reform.data };
+    if (
+      Object.keys(data).length === 0 &&
+      Object.keys(newReforms).length === 1
+    ) {
+      let newSearch = copySearchParams(searchParams);
+      newSearch.delete("reform");
+      setSearchParams(newSearch);
+    } else {
+      newReforms[parameterName] = data;
+      getNewPolicyId(metadata.countryId, newReforms).then((result) => {
+        if (result.status !== "ok") {
+          console.error(
+            "ParameterEditor: In attempting to fetch new " +
+              "policy, the following error occurred: " +
+              result.message,
+          );
+        } else {
+          let newSearch = copySearchParams(searchParams);
+          newSearch.set("reform", result.policy_id);
+          setSearchParams(newSearch);
+        }
+      });
+    }
+  }
+
+  if (parameter.unit === "bool" || parameter.unit === "abolition") {
+    return (
+      <div style={{ padding: 10 }}>
+        <Switch
+          key={"input for" + parameter.parameter}
+          defaultChecked={startValue}
+          onChange={(value) => changeHandler(!!value)}
         />
       </div>
-    </CenteredMiddleColumn>
-  );
+    );
+  } else {
+    const isPercent = parameter.unit === "/1";
+    const scale = isPercent ? 100 : 1;
+    const isCurrency = Object.keys(currencyMap).includes(parameter.unit);
+    const maximumFractionDigits = isCurrency ? 2 : 16;
+    return (
+      <StableInputNumber
+        style={{
+          width: displayCategory === "mobile" ? "50%" : "100%",
+        }}
+        key={"input for" + parameter.parameter}
+        {...(isCurrency
+          ? {
+              addonBefore: currencyMap[parameter.unit],
+            }
+          : {})}
+        {...(isPercent
+          ? {
+              addonAfter: "%",
+            }
+          : {})}
+        formatter={(value, { userTyping }) => {
+          const n = +value;
+          const isInteger = Number.isInteger(n);
+          return n.toLocaleString(localeCode(metadata.countryId), {
+            minimumFractionDigits: userTyping || isInteger ? 0 : 2,
+            maximumFractionDigits: userTyping ? 16 : maximumFractionDigits,
+          });
+        }}
+        defaultValue={Number(startValue) * scale}
+        onPressEnter={(_, value) =>
+          changeHandler(+value.toFixed(maximumFractionDigits) / scale)
+        }
+      />
+    );
+  }
 }
 
 /**
