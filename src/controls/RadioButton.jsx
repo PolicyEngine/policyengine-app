a4e0bcecb659ab0d54fdb0cd3839b4c18561af27
daemon-reconfig <mehulishi27@gmail.com>
2024-04-20 17:40:50 +0530

    Changed border color to white

--- src/controls/RadioButton.jsx
+++ src/controls/RadioButton.jsx
@@ -7,13 +7,15 @@ export default function RadioButton(props) {
   // Create options array for Radio.Group
   const options = keys.map((key, index) => ({
     label: labels[index],
-    value: key,
-    style: {
+    value: key,    style: {
       fontSize: "16px",
       borderRadius: "0px",
       backgroundColor:
         index === 0 ? style.colors.BLUE : style.colors.MEDIUM_DARK_GRAY,
       color: index === 0 ? style.colors.WHITE : style.colors.DARK_GRAY,
+      border: "solid",
+      borderColor: 
+        index === 0 ? style.colors.BLUE : style.colors.WHITE,
     },
   }));
 
