a86ba38497bdf122364bf83520da76ee53b2c5be
daemon-reconfig <mehulishi27@gmail.com>
2024-04-27 02:16:00 +0530

    Added Radio.Button and Fixed RadioButton.jsx

--- src/controls/RadioButton.jsx
+++ src/controls/RadioButton.jsx
@@ -1,32 +1,42 @@
+"use client";
 import { Radio } from "antd";
 import style from "../style";
+import { useState } from "react";
 
 export default function RadioButton(props) {
-  const { keys, labels, onChange, value } = props;
+  const { keys, labels, onChange, defaultValue } = props;
 
+  const [value, setValue] = useState(defaultValue || keys[0]);
+  function handleChange(e) {  
+    if (onChange instanceof Function) {
+      onChange(e.target.value);
+    }
+    setValue(e.target.value);
+   
+  }
   // Create options array for Radio.Group
-  const options = keys.map((key, index) => ({
-    label: labels[index],
-    value: key,
-    style: {
-      fontSize: "16px",
-      borderRadius: "0px",
-      backgroundColor:
-        index === 0 ? style.colors.BLUE : style.colors.MEDIUM_DARK_GRAY,
-      color: index === 0 ? style.colors.WHITE : style.colors.DARK_GRAY,
-      border: "solid",
-      borderColor: index === 0 ? style.colors.BLUE : style.colors.WHITE,
-    },
-  }));
-
+  const options = keys.map((key, index) => (
+    <Radio.Button
+      key={key}
+      value={key}
+      style={{
+        fontSize: "16px",
+        border: "none",
+        borderRadius: "0px",
+        backgroundColor:
+          key === value ? style.colors.BLUE : style.colors.MEDIUM_DARK_GRAY,
+        color: key === value ? style.colors.WHITE : style.colors.DARK_GRAY,
+        border: "solid",
+        borderColor: key === value ? style.colors.BLUE : style.colors.WHITE,
+      }}
+      checked={key === value}
+    >
+      {labels[index]}
+    </Radio.Button>
+  ));
   return (
-    <Radio.Group
-      options={options}
-      onChange={onChange}
-      value={value || keys[0]}
-      optionType="button"
-      buttonStyle="solid"
-      size="large"
-    />
+    <Radio.Group size="large" onChange={handleChange} >
+      {options}
+    </Radio.Group>
   );
 }
