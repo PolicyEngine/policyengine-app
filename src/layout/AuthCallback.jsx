1e494dab1f0c9f46bdb3834cbecf6da07ca357b4
Anthony Volk <anth.volk@gmail.com>
2024-04-30 13:45:30 +0200

    fix: Set explicit title on callback page

--- src/layout/AuthCallback.jsx
+++ src/layout/AuthCallback.jsx
@@ -1,6 +1,14 @@
+import { Helmet } from "react-helmet";
 import Header from "../redesign/components/Header";
 
 // Empty component for smoothing post-login redirects visually
 export default function AuthCallback() {
-  return <Header />;
+  return (
+    <>
+      <Helmet>
+        <title>PolicyEngine</title>
+      </Helmet>
+      <Header />
+    </>
+  );
 }
