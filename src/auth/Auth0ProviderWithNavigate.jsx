c6597079c92e4a498e34e129d3412f2b4108d34d
anmolchhabra21 <anmolchhabra71500@gmail.com>
2024-04-30 17:28:57 +0530

    Added conditional redirect_uri if running in debug mode

--- src/auth/Auth0ProviderWithNavigate.jsx
+++ src/auth/Auth0ProviderWithNavigate.jsx
@@ -4,9 +4,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 export default function Auth0ProviderWithNavigate({ children }) {
   const navigate = useNavigate();
 
-  const redirectUri = "https://policyengine.org/callback";
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const redirectUri = process.env.REACT_APP_DEBUG
+    ? "http://localhost:3000/callback"
+    : "https://policyengine.org/callback";
 
   const onRedirectCallback = (appState) => {
     navigate(appState?.returnTo || window.location.pathname);
