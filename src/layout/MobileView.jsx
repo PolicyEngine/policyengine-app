ef22e554c7b9d7eb23f08c7bb3b945db6c0914a7
Anthony Volk <anth.volk@gmail.com>
2024-05-02 18:10:08 +0200

    fix: Remove unused code

--- src/layout/MobileView.jsx
+++
@@ -1,5 +0,0 @@
-export default function MobileView(props) {
-  // Uses CSS to only show the mobile view when the screen is small enough.
-  const { children } = props;
-  return <div className="d-lg-none">{children}</div>;
-}
