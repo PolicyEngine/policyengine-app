e7e8394a86f5cfa5eabede6ab68496bde40be2f6
Anthony Volk <anth.volk@gmail.com>
2024-05-06 19:05:36 +0200

    test: Template for policyengine-test

---
+++ src/__tests__/PolicyEngine.test.js
@@ -0,0 +1,56 @@
+import "@testing-library/jest-dom";
+import { render, waitFor } from "@testing-library/react";
+import { BrowserRouter, useSearchParams } from "react-router-dom";
+import fetch from "node-fetch";
+
+import PolicyEngine from "../redesign/components/PolicyEngine";
+
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+  return {
+    __esModule: true,
+    ...originalModule,
+    useSearchParams: jest.fn(),
+  };
+});
+
+let metadataUS = null;
+let metadataUK = null;
+beforeAll(async () => {
+  const res = await fetch("https://api.policyengine.org/us/metadata");
+  const metadataRaw = await res.json();
+  metadataUS = metadataRaw.result;
+
+  const resUK = await fetch("https://api.policyengine.org/uk/metadata");
+  const metadataRawUK = await resUK.json();
+  metadataUK = metadataRawUK.result;
+});
+
+describe("Test main PolicyEngine component", () => {
+  test("Renders for US if proper data passed");
+  test("Renders for UK if proper data passed");
+  test("Metadata re-fetches if country ID changes");
+  test("Fetches baseline policy data");
+  test("Fetches reform policy data");
+  test("Fetches user profile if user is logged in");
+  test("Redirects from / to /[countryId]");
+  test("Loads auth callback");
+  test("Loads about page");
+  test("Loads jobs page");
+  test("Loads testimonials page");
+  test("Loads calculator interstitial page");
+  test("Loads research page");
+  test("Loads contact page");
+  test("Loads donate page");
+  test("Loads individual blog posts");
+  test("Loads privacy page");
+  test("Loads T&C page");
+  test("Loads household page");
+  test("Loads policy page");
+  test("Loads user profile page");
+  test("Loads API documentation page");
+  test("Loads TRAFWA calculator");
+  test("Loads Citizens Economic Council page");
+  test("Redirects from /countryId/blog/slug to /countryId/research/slug");
+  test("Redirects for unrecognized paths");
+})
