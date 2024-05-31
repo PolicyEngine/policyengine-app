50cd8dbf9de01d0d43cc2b4c827360e0bd2d6372
Sahil <issahil404@gmail.com>
2024-05-31 06:48:23 +0530

    added test cases for founders and team secion, also added test for jobs link

--- src/__tests__/pages/About.test.js
+++ src/__tests__/pages/About.test.js
@@ -12,12 +12,23 @@ describe("About Page", () => {
       </BrowserRouter>,
     );
 
+    // Check for the PageHeader title
     const heading = screen.getByRole("heading", { name: /Our people/i });
-
     expect(heading).toBeInTheDocument();
-    const max = screen.getByText(
-      "PolicyEngine's team leads a global movement of open-source contributors.",
-    );
-    expect(max).toBeInTheDocument();
+
+    // Check for the section with a heading "Founders"
+    const foundersSection = screen.getByRole("heading", { name: /Founders/i });
+    expect(foundersSection).toBeInTheDocument();
+
+    // Check for the section with a heading "Team"
+    const teamSection = screen.getByRole("heading", { name: /Team/i });
+    expect(teamSection).toBeInTheDocument();
+
+    // Check that the link to jobs page exists
+    const link = screen.getByRole("link", {
+      name: /Learn about opportunities to join us./i,
+    });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", expect.stringContaining("/jobs"));
   });
 });
