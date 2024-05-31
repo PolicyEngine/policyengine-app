65a52bd01db2c349acc04dbd0af081e57bab200e
Sahil <issahil404@gmail.com>
2024-05-31 06:31:53 +0530

    removed debug method, added browser-router

--- src/__tests__/pages/About.test.js
+++ src/__tests__/pages/About.test.js
@@ -2,14 +2,14 @@ import React from "react";
 import "@testing-library/jest-dom";
 import { screen, render } from "@testing-library/react";
 import About from "../../pages/About";
-import { MemoryRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 describe("About Page", () => {
   test("renders without crashing", () => {
     render(
-      <MemoryRouter>
+      <BrowserRouter>
         <About />
-      </MemoryRouter>,
+      </BrowserRouter>,
     );
 
     const heading = screen.getByRole("heading", { name: /Our people/i });
@@ -19,7 +19,5 @@ describe("About Page", () => {
       "PolicyEngine's team leads a global movement of open-source contributors.",
     );
     expect(max).toBeInTheDocument();
-
-    screen.debug(max);
   });
 });
