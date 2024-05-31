7df17e5e6f4da86c1d73a33517d97e6488fb66c1
Sahil <issahil404@gmail.com>
2024-06-01 03:10:22 +0530

    added assertions for heading and subheading

--- src/__tests__/pages/Research.test.js
+++ src/__tests__/pages/Research.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import Research from "../../pages/Research";
 import { BrowserRouter } from "react-router-dom";
 
@@ -11,5 +11,13 @@ describe("Research Page", () => {
         <Research />
       </BrowserRouter>,
     );
+    const heading = screen.getByRole("heading", {
+      name: /Research and analysis/i,
+    });
+    expect(heading).toBeInTheDocument();
+    const subheadingText =
+      "Read PolicyEngine's research on recent and proposed policy reforms, as well as technical and general updates from the organisation.";
+    const subheading = screen.getByText(subheadingText);
+    expect(subheading).toBeInTheDocument();
   });
 });
