8b02be36f3e63d7f0b9ec1feb109de95e7bb92ec
Max Ghenis <mghenis@gmail.com>
2024-09-29 11:57:52 -0400

    update staff

--- src/data/staff.js
+++ src/data/staff.js
@@ -1,96 +1,96 @@
-// A JSON data structure that describes PolicyEngine founders,
+// A JSON data structure that describes PolicyEngine staff,
 // for use on the About Us page
 
 export const founders = {
   max_ghenis: {
     name: "Max Ghenis",
-    bio: `is the co-founder and CEO of PolicyEngine. He is
-		also the founder and president of the UBI Center, a think tank
-		researching universal basic income policies, and was previously a
-		data scientist at Google. Max has a master's degree in Data,
-		Economics, and Development Policy from MIT and a bachelor's
-		degree in operations research from UC Berkeley.`,
+    bio: `is the co-founder and CEO of PolicyEngine. He previously founded and
+served as president of the UBI Center, a think tank researching universal basic
+income policies, and worked at Google as a data scientist, leading projects to
+make internal operations and products more inclusive across the world. Max
+earned a bachelor's degree in operations research from UC Berkeley and a
+master's degree in Data, Economics, and Development Policy from MIT.`,
     image: require("../images/authors/max-ghenis.png"),
   },
   nikhil_woodruff: {
     name: "Nikhil Woodruff",
-    bio: `is the co-founder and CTO of PolicyEngine. He is
-    also the UK Research Director at the UBI Center, a think tank
-    researching universal basic income policies, and was previously a data
-    scientist at Caspian, where he worked in improving anti-money laundering
-    investigations. Nikhil has a bachelor's degree in Computer Science from the
-    University of Durham.`,
-    image: require("../images/authors/nikhil-woodruff.jpg"),
+    bio: `is the co-founder and CTO of PolicyEngine. He previously served as UK
+Research Director at the UBI Center, a think tank researching universal basic
+income policies, and worked at Caspian as a data scientist, improving
+anti-money laundering investigations. Nikhil earned a bachelor's degree in
+Computer Science from the University of Durham.`,
+    image: require("../images/authors/nikhil-woodruff.png"),
   },
 };
 
 export const staff = {
   pavel_makarchuk: {
     name: "Pavel Makarchuk",
-    bio: `is a Policy Modeling Manager at PolicyEngine. He has 
-    graduated with a Bachelor’s degree in Economics from Marist College 
-    where he researched the economic implications of forced migration. 
-    Pavel is currently enrolled in the International Economics Master’s 
-    program at the Hochschule für Wirtschaft und Recht in Berlin.`,
+    bio: `is a Policy Modeling Manager at PolicyEngine. He earned a bachelor’s
+degree in Economics from Marist College where he researched the economic
+implications of forced migration.`,
     image: require("../images/authors/pavel-makarchuk.png"),
   },
   anthony_volk: {
     name: "Anthony Volk",
-    bio: `is a full-stack engineer at PolicyEngine. He has a passion 
-    for using full-stack technologies and web development best 
-    practices to build digital tools that empower society. Previously, 
-    he conducted research into international infrastructure and digital 
-    development with the Institute for State Effectiveness. He holds an
-    A.B. in Government and East Asian Studies from Harvard 
-    University.`,
+    bio: `is a full-stack engineer at PolicyEngine. He has a passion for using
+full-stack technologies and web development best practices to build digital
+tools that empower society. Previously, he conducted research into
+international infrastructure and digital development with the Institute for
+State Effectiveness. Anthony earned bachelor's degrees in Government and East
+Asian Studies from Harvard University.`,
     image: require("../images/authors/anthony-volk.jpg"),
+  },
+  leigh_gibson: {
+    name: "Leigh Gibson",
+    bio: `is a strategy consultant at PolicyEngine. Previously she was Vice
+President of Social Policy at the Niskanen Center, Senior Policy Officer at the
+Robert Wood Johnson Foundation, and Senior Director of State Network
+Advancement at the Center on Budget and Policy Priorities. Leigh earned a
+bachelor's degree in Religion from Princeton University, a master's degree in
+Social Work from Hunter College, and a Ph.D. in Religion/Religious Studies from
+Princeton University.`,
+    image: require("../images/authors/leigh-gibson.png"),
   },
   martin_holmer: {
     name: "Martin Holmer",
-    bio: `is an economist at PolicyEngine (consultant). 
-    He focuses on our state income tax models and validation architecture, 
-    and has contributed other models such as the premium tax credit. 
-    Previously, he was the lead developer for the Policy Simulation Library's Tax-Calculator federal income tax microsimulation model, 
-    and he has built microsimulation models around the world for organizations including the World Bank and the US Social Security Administration. 
-    Martin has a Ph.D. in economics from MIT.`,
-    image: require("../images/authors/martin_holmer.png"),
+    bio: `is an economics consultant at PolicyEngine. He focuses on our state
+income tax models and validation architecture, and has contributed other models
+such as the premium tax credit. Previously, he was the lead developer for the
+Policy Simulation Library's Tax-Calculator federal income tax microsimulation
+model, and he has built microsimulation models around the world for
+organizations including the World Bank and the US Social Security
+Administration. Martin earned a Ph.D. in economics from MIT.`,
+    image: require("../images/authors/martin-holmer.png"),
   },
 };
 
 export const advisors = {
-  george_sadowsky: {
-    name: "George Sadowsky",
-    bio: `has previously served on boards of The Internet Society and ICANN. 
-    George has held senior leadership positions at the Urban Institute, the 
-    Brookings Institution, the United Nations, and other international 
-    organizations building microsimulation and computing systems for 
-    policymaking. George holds an A.B. in Mathematics from Harvard University, 
-    and an M.A. and Ph.D. in Economics from Yale University.`,
-    image: require("../images/headshots/george_sadowsky.jpeg"),
+  tim_shaw: {
+    name: "Tim Shaw",
+    bio: `is the Policy Director at the Aspen Institute Financial Security
+Program. He previously worked as an Associate Director of Economic Policy at
+the Bipartisan Policy Center as an analyst at the Government Accountability
+Office. Tim earned bachelor's degrees in Political Science and Spanish at
+Washington University in St. Louis and a master's degree in Public Policy and
+Management from Carnegie Mellon University.`,
+    image: require("../images/authors/tim-shaw.png"),
   },
-  damiola_ogundipe: {
+  damola_ogundipe: {
     name: "Damola Ogundipe",
-    bio: `is the co-founder and CEO of Plural Policy, a software company that 
-    helps government relations and public policy teams influence and shape 
-    public policy. He previously worked as a Senior Consultant at Oxford 
-    Strategic Consulting. Damola holds a B.S. in Finance from the University 
-    of Minnesota.`,
-    image: require("../images/headshots/damiola_ogundipe.jpeg"),
+    bio: `is the co-founder and CEO of Plural Policy, a software company that
+helps government relations and public policy teams influence and shape public
+policy. He previously worked as a Senior Consultant at Oxford Strategic
+Consulting. Damola earned a bachelor's degree in Finance from the University of
+Minnesota.`,
+    image: require("../images/headshots/damola-ogundipe.jpeg"),
   },
   jesse_horwitz: {
     name: "Jesse Horwitz",
-    bio: `is the co-founder of Agora and Hubble Contacts. He previously worked 
-    as a policy advisor to Andrew Yang's mayoral campaign and as an investment 
-    analyst. Jesse holds a bachelor's degree in Economics-Mathematics from 
-    Columbia University and attended Harvard Law School.`,
-    image: require("../images/headshots/jesse_horwitz.jpeg"),
-  },
-  matt_jensen: {
-    name: "Matt Jensen",
-    bio: `is the founding director of the Open Source Policy Center at the 
-    American Enterprise Institute. He is also a co-founder of the Open Research 
-    Group, the Policy Simulation Library, and Compute Tooling. Jensen is a 
-    graduate of Pomona College with a degree in math.`,
-    image: require("../images/headshots/matt_jensen.png"),
+    bio: `is the co-founder of Agora and Hubble Contacts. He previously worked as
+a policy advisor to Andrew Yang's mayoral campaign and as an investment
+analyst. Jesse earned a bachelor's degree in Economics-Mathematics from
+Columbia University and attended Harvard Law School.`,
+    image: require("../images/headshots/jesse-horwitz.jpeg"),
   },
 };
