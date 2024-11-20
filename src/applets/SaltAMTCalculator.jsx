d2105c158d5146958f7c5df711034e5462ead15d
PavelMakarchuk <pavel.ma99@gmail.com>
2024-11-20 17:24:18 -0500

    format

--- src/applets/SaltAMTCalculator.jsx
+++ src/applets/SaltAMTCalculator.jsx
@@ -6,28 +6,28 @@ import { useWindowHeight } from "../hooks/useWindow";
 export default function SaltAMTCalculator() {
   const windowHeight = useWindowHeight();
 
-    return (
-      <>
-        <Helmet>
-          <title>SALT-AMT Calculator | PolicyEngine</title>
-          </Helmet>
-        <Header />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            height: windowHeight - style.spacing.HEADER_HEIGHT,
-            width: "100vw",
-          }}
-        >
-          <iframe
-            src="https://policyengine-salt-amt-calculator.streamlit.app/?embedded=true"
-            title="SALT-AMT Policy Reform Calculator"
-            height={`calc(100vh - ${style.spacing.HEADER_HEIGHT})`}
-            width="100%"
-            style={{ overflow: "hidden" }}
-          />
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <Helmet>
+        <title>SALT-AMT Calculator | PolicyEngine</title>
+      </Helmet>
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: windowHeight - style.spacing.HEADER_HEIGHT,
+          width: "100vw",
+        }}
+      >
+        <iframe
+          src="https://policyengine-salt-amt-calculator.streamlit.app/?embedded=true"
+          title="SALT-AMT Policy Reform Calculator"
+          height={`calc(100vh - ${style.spacing.HEADER_HEIGHT})`}
+          width="100%"
+          style={{ overflow: "hidden" }}
+        />
+      </div>
+    </>
+  );
+}
