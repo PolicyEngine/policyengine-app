fab139db8e465a3423e519f7474881b1b95e78e9
harnish7576 <savadiaharnish@gmail.com>
2025-03-03 18:40:40 -0500

    added undefined functions

--- src/__tests__/pages/policy/PolicySearch.test.js
+++ src/__tests__/pages/policy/PolicySearch.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 import { baselinePolicyUS } from "../../__setup__/sampleData";
