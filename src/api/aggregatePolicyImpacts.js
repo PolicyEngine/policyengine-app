2df00183ad9082e989af5d08f31696289dd3ebb3
Anthony Volk <anth.volk@gmail.com>
2025-04-09 13:01:38 -0700

    feat: Aggregate LSRs

--- src/api/aggregatePolicyImpacts.js
+++ src/api/aggregatePolicyImpacts.js
@@ -29,8 +29,8 @@ export async function aggregateSocietyWideImpacts(impacts) {
     /*
     intra_decile: aggregateIntraDecileData(impacts.map(impact => impact.intra_decile)),
     intra_wealth_decile: null, // Placeholder for intra wealth decile aggregation
-    labor_supply_response: aggregateLaborSupplyResponseData(impacts.map(impact => impact.labor_supply_response)),
     */
+    labor_supply_response: aggregateLaborSupplyData(impacts.map(impact => impact.labor_supply_response)),
     poverty: aggregatePovertyData(impacts.map(impact => impact.poverty)),
     poverty_by_gender: aggregatePovertyByGenderData(impacts.map(impact => impact.poverty_by_gender)),
     poverty_by_race: aggregatePovertyByRaceData(impacts.map(impact => impact.poverty_by_race)),
@@ -149,6 +149,36 @@ function aggregatePovertyByRaceBreakdown(raceBreakdowns) {
     white: aggregateBaselineReformComparison(raceBreakdowns.map(b => b?.white), 'mean', 'mean'),
     other: aggregateBaselineReformComparison(raceBreakdowns.map(b => b?.other), 'mean', 'mean'),
   }
+}
+
+function aggregateLaborSupplyData(laborSupplyData) {
+  return {
+    hours: {
+      baseline: aggregateValues(laborSupplyData.map(l => l?.hours?.baseline)),
+      change: aggregateValues(laborSupplyData.map(l => l?.hours?.change)),
+      income_effect: aggregateValues(laborSupplyData.map(l => l?.hours?.income_effect)),
+      reform: aggregateValues(laborSupplyData.map(l => l?.hours?.reform)),
+      substitution_effect: aggregateValues(laborSupplyData.map(l => l?.hours?.substitution_effect)),
+    },
+    income_lsr: aggregateValues(laborSupplyData.map(l => l?.income_lsr)), 
+    substitution_lsr: aggregateValues(laborSupplyData.map(l => l?.substitution_lsr)),
+    total_change: aggregateValues(laborSupplyData.map(l => l?.total_change)),
+    revenue_change: aggregateValues(laborSupplyData.map(l => l?.revenue_change)),
+    decile: {
+      average: {
+        income: aggregateDecileComparison(laborSupplyData.map(l => l?.decile?.average?.income)),
+        substitution: aggregateDecileComparison(laborSupplyData.map(l => l?.decile?.average?.substitution)),
+      },
+      relative: {
+        income: aggregateDecileComparison(laborSupplyData.map(l => l?.decile?.relative?.income), 'mean'),
+        substitution: aggregateDecileComparison(laborSupplyData.map(l => l?.decile?.relative?.substitution), 'mean'),
+      },
+    },
+    relative_lsr: {
+      income: aggregateValues(laborSupplyData.map(l => l?.relative_lsr?.income), 'mean'),
+      substitution: aggregateValues(laborSupplyData.map(l => l?.relative_lsr?.substitution), 'mean'),
+    },
+  };
 }
 
 /**
