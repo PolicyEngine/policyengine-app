fcc47d6dcd1931efc7bd83d6d8c56f2f6db54059
Anthony Volk <anth.volk@gmail.com>
2025-04-10 17:52:21 -0700

    chore: Test validateImpacts

--- src/__tests__/api/societyWideAggregation/aggregate.test.js
+++ src/__tests__/api/societyWideAggregation/aggregate.test.js
@@ -2,6 +2,7 @@ import {
   aggregateSocietyWideImpacts,
   aggregateSocietyWideImpactsUK,
   aggregateSocietyWideImpactsUS,
+  validateImpacts,
 } from "../../../api/societyWideAggregation/aggregate";
 import { SocietyWideImpactUS } from "../../../schemas/societyWideImpact";
 import {
@@ -81,6 +82,49 @@ describe("aggregateSocietyWideImpactsUK", () => {
       expect(() => aggregateSocietyWideImpactsUK(impacts)).toThrow(
         "Cannot aggregate empty or undefined impacts",
       );
+    });
+  });
+});
+
+describe("validateImpacts", () => {
+  describe("Given a valid US impact and US country ID", () => {
+    test("it should return true", () => {
+      const impact = testObjectsUS[0];
+      const countryId = "us";
+
+      expect(validateImpacts(countryId, impact)).toBe(true);
+    });
+  });
+  describe("Given a valid UK impact and UK country ID", () => {
+    test("it should return true", () => {
+      const impact = testObjectsUK[0];
+      const countryId = "uk";
+
+      expect(validateImpacts(countryId, impact)).toBe(true);
+    });
+  });
+  describe("Given a valid US impact and UK country ID", () => {
+    test("it should return false", () => {
+      const impact = testObjectsUS[0];
+      const countryId = "uk";
+
+      expect(validateImpacts(countryId, impact)).toBe(false);
+    });
+  });
+  describe("Given an invalid country ID and US impact", () => {
+    test("it should return false", () => {
+      const impact = testObjectsUS[0];
+      const countryId = "invalid_country";
+
+      expect(validateImpacts(countryId, impact)).toBe(false);
+    });
+  });
+  describe("Given a valid country ID and invalid impact", () => {
+    test("it should return false", () => {
+      const impact = {};
+      const countryId = "uk";
+
+      expect(validateImpacts(countryId, impact)).toBe(false);
     });
   });
 });
