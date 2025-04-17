fa3dc7bce7476138922b51772e6ef3984656fd19
Anthony Volk <anth.volk@gmail.com>
2025-04-17 19:40:26 -0400

    feat: Disable options in multi-year runs

--- src/layout/StackedMenu.jsx
+++ src/layout/StackedMenu.jsx
@@ -1,13 +1,32 @@
+import { useSearchParams } from "react-router-dom";
 import Menu from "./Menu";
 
 export default function StackedMenu(props) {
   const { firstTree, secondTree, selected, onSelect } = props;
 
+  const [searchParams] = useSearchParams();
+  const isMultiYear =
+    searchParams.get("simYears") &&
+    !Number.isNaN(searchParams.get("simYears")) &&
+    searchParams.get("simYears") > 1;
+
   const isOnOutput =
     window.location.search.includes("focus=policyOutput") ||
     window.location.search.includes("focus=householdOutput");
+
+  const isOnMultiYearOutput = isOnOutput && isMultiYear;
+
   let result;
-  if (isOnOutput) {
+  if (isOnMultiYearOutput) {
+    result = (
+      <Menu
+        tree={secondTree}
+        selected={selected}
+        onSelect={onSelect}
+        isMultiYear
+      />
+    );
+  } else if (isOnOutput) {
     result = <Menu tree={secondTree} selected={selected} onSelect={onSelect} />;
   } else {
     result = <Menu tree={firstTree} selected={selected} onSelect={onSelect} />;
