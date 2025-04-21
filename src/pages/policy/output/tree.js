edaf33789d4f3222b7a514f7629f5475a4f78c3e
Anthony Volk <anth.volk@gmail.com>
2025-04-21 17:58:24 -0400

    fix: Alter how disabling of policy output tree items is done

--- src/pages/policy/output/tree.js
+++ src/pages/policy/output/tree.js
@@ -51,6 +51,13 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
       ? searchParams.get("uk_local_areas_beta") === "true"
       : false;
 
+  const isMultiYear =
+    searchParams && typeof searchParams.get === "function"
+      ? searchParams.get("simYears") &&
+        !Number.isNaN(searchParams.get("simYears")) &&
+        searchParams.get("simYears") > 1
+      : false;
+
   const tree = [
     {
       name: "policyOutput",
@@ -59,50 +66,63 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
         {
           name: "policyOutput.policyBreakdown",
           label: "Overview",
+          disabled: isMultiYear,
         },
-        {
-          name: "policyOutput.budgetaryImpact",
-          label: "Budgetary impact",
-          children: [
-            {
-              name: "policyOutput.budgetaryImpact.overall",
-              label: "Overall",
+        isMultiYear
+          ? {
+              name: "policyOutput.budgetaryImpact",
+              label: "Budgetary impact",
+            }
+          : {
+              name: "policyOutput.budgetaryImpact",
+              label: "Budgetary impact",
+              children: [
+                {
+                  name: "policyOutput.budgetaryImpact.overall",
+                  label: "Overall",
+                },
+                countryId === "uk" && {
+                  name: "policyOutput.budgetaryImpact.byProgram",
+                  label: "By program",
+                },
+              ].filter((x) => x),
             },
-            countryId === "uk" && {
-              name: "policyOutput.budgetaryImpact.byProgram",
-              label: "By program",
-            },
-          ].filter((x) => x),
-        },
         {
           name: "policyOutput.distributionalImpact",
           label: "Distributional impact",
+          disabled: isMultiYear,
           children: [
             {
               name: "policyOutput.distributionalImpact.incomeDecile",
               label: "By income decile",
+              disabled: isMultiYear,
               children: [
                 {
                   name: "policyOutput.distributionalImpact.incomeDecile.relative",
                   label: "Relative",
+                  disabled: isMultiYear,
                 },
                 {
                   name: "policyOutput.distributionalImpact.incomeDecile.average",
                   label: "Average",
+                  disabled: isMultiYear,
                 },
               ],
             },
             countryId === "uk" && {
               name: "policyOutput.distributionalImpact.wealthDecile",
               label: "By wealth decile",
+              disabled: isMultiYear,
               children: [
                 {
                   name: "policyOutput.distributionalImpact.wealthDecile.relative",
                   label: "Relative",
+                  disabled: isMultiYear,
                 },
                 {
                   name: "policyOutput.distributionalImpact.wealthDecile.average",
                   label: "Average",
+                  disabled: isMultiYear,
                 },
               ],
             },
@@ -111,14 +131,17 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
         {
           name: "policyOutput.winnersAndLosers",
           label: "Winners and losers",
+          disabled: isMultiYear,
           children: [
             {
               name: "policyOutput.winnersAndLosers.incomeDecile",
               label: "By income decile",
+              disabled: isMultiYear,
             },
             countryId === "uk" && {
               name: "policyOutput.winnersAndLosers.wealthDecile",
               label: "By wealth decile",
+              disabled: isMultiYear,
             },
             uk_local_areas_beta && {
               name: "policyOutput.winnersAndLosers.constituencies",
@@ -129,36 +152,44 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
         {
           name: "policyOutput.povertyImpact",
           label: "Poverty impact",
+          disabled: isMultiYear,
           children: [
             {
               name: "policyOutput.povertyImpact.regular",
               label: "Regular poverty",
+              disabled: isMultiYear,
               children: [
                 {
                   name: "policyOutput.povertyImpact.regular.byAge",
                   label: "By age",
+                  disabled: isMultiYear,
                 },
                 countryId === "us" && {
                   name: "policyOutput.povertyImpact.regular.byRace",
                   label: "By race",
+                  disabled: isMultiYear,
                 },
                 {
                   name: "policyOutput.povertyImpact.regular.byGender",
                   label: "By gender",
+                  disabled: isMultiYear,
                 },
               ].filter((x) => x),
             },
             {
               name: "policyOutput.povertyImpact.deep",
               label: "Deep poverty",
+              disabled: isMultiYear,
               children: [
                 {
                   name: "policyOutput.povertyImpact.deep.byAge",
                   label: "By age",
+                  disabled: isMultiYear,
                 },
                 {
                   name: "policyOutput.povertyImpact.deep.byGender",
                   label: "By gender",
+                  disabled: isMultiYear,
                 },
               ],
             },
@@ -167,22 +198,27 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
         {
           name: "policyOutput.inequalityImpact",
           label: "Inequality impact",
+          disabled: isMultiYear,
         },
         {
           name: "policyOutput.cliffImpact",
           label: "Cliff impact",
+          disabled: isMultiYear,
         },
         uk_local_areas_beta && {
           name: "policyOutput.constituencies",
           label: "Parliamentary constituencies (experimental)",
+          disabled: isMultiYear,
           children: [
             {
               name: "policyOutput.constituencies.relative",
               label: "Relative change",
+              disabled: isMultiYear,
             },
             {
               name: "policyOutput.constituencies.average",
               label: "Average change",
+              disabled: isMultiYear,
             },
           ],
         },
@@ -192,70 +228,86 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
             countryId === "uk"
               ? "Labour supply impact (experimental)"
               : "Labor supply impact (experimental)",
+          disabled: isMultiYear,
           children: [
             uk_local_areas_beta && {
               name: "policyOutput.constituencies.laborSupplyFTEs",
               label: "By parliamentary constituency",
+              disabled: isMultiYear,
             },
             countryId === "us" && {
               name: "policyOutput.laborSupplyImpact.hours",
               label: "Hours worked",
+              disabled: isMultiYear,
             },
             {
               name: "policyOutput.laborSupplyImpact.earnings",
               label: "Earnings",
+              disabled: isMultiYear,
               children: [
                 {
                   name: "policyOutput.laborSupplyImpact.earnings.overall",
                   label: "Overall",
+                  disabled: isMultiYear,
                   children: [
                     {
                       name: "policyOutput.laborSupplyImpact.earnings.overall.relative",
                       label: "Relative",
+                      disabled: isMultiYear,
                     },
                     {
                       name: "policyOutput.laborSupplyImpact.earnings.overall.absolute",
                       label: "Absolute",
+                      disabled: isMultiYear,
                     },
                   ],
                 },
                 {
                   name: "policyOutput.laborSupplyImpact.earnings.byDecile",
                   label: "By decile",
+                  disabled: isMultiYear,
                   children: [
                     {
                       name: "policyOutput.laborSupplyImpact.earnings.byDecile.relative",
                       label: "Relative",
+                      disabled: isMultiYear,
                       children: [
                         {
                           name: "policyOutput.laborSupplyImpact.earnings.byDecile.relative.total",
                           label: "Total",
+                          disabled: isMultiYear,
                         },
                         {
                           name: "policyOutput.laborSupplyImpact.earnings.byDecile.relative.income",
                           label: "Income effect",
+                          disabled: isMultiYear,
                         },
                         {
                           name: "policyOutput.laborSupplyImpact.earnings.byDecile.relative.substitution",
                           label: "Substitution effect",
+                          disabled: isMultiYear,
                         },
                       ],
                     },
                     {
                       name: "policyOutput.laborSupplyImpact.earnings.byDecile.absolute",
                       label: "Absolute",
+                      disabled: isMultiYear,
                       children: [
                         {
                           name: "policyOutput.laborSupplyImpact.earnings.byDecile.absolute.total",
                           label: "Total",
+                          disabled: isMultiYear,
                         },
                         {
                           name: "policyOutput.laborSupplyImpact.earnings.byDecile.absolute.income",
                           label: "Income effect",
+                          disabled: isMultiYear,
                         },
                         {
                           name: "policyOutput.laborSupplyImpact.earnings.byDecile.absolute.substitution",
                           label: "Substitution effect",
+                          disabled: isMultiYear,
                         },
                       ],
                     },
@@ -268,10 +320,12 @@ export function getPolicyOutputTree(countryId, searchParams = {}) {
         {
           name: "policyOutput.analysis",
           label: "AI summary (experimental)",
+          disabled: isMultiYear,
         },
         {
           name: "policyOutput.codeReproducibility",
           label: "Reproduce in Python",
+          disabled: isMultiYear,
         },
       ].filter((x) => x),
     },
