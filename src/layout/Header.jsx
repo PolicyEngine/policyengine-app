afa5c63724d0a98e1bdb15cf3ef3f325afe53c84
siddhartha <dashranger60@gmail.com>
2025-05-20 07:09:19 +0000

    text visible on hover

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -599,7 +599,7 @@ const MobileStandardLink = ({ link, countryId, isOpen, index }) => {
       <HoverBox
         hoverStart="left"
         size="100vw"
-        hoverBackgroundColor={style.colors.WHITE}
+        hoverBackgroundColor={style.colors.BLUE_PRESSED}
         style={{
           margin: 30,
         }}
@@ -667,7 +667,7 @@ const MobileDropdownHeader = ({
     <HoverBox
       hoverStart="left"
       size="100vw"
-      hoverBackgroundColor={style.colors.WHITE}
+      hoverBackgroundColor={style.colors.BLUE_PRESSED}
       style={{
         margin: 30,
       }}
@@ -730,7 +730,7 @@ const MobileDropdownItem = ({ item, countryId, index }) => {
       <HoverBox
         hoverStart="left"
         size="100vw"
-        hoverBackgroundColor={style.colors.WHITE}
+        hoverBackgroundColor={style.colors.BLUE_PRESSED}
         style={{
           margin: "15px 30px",
         }}
