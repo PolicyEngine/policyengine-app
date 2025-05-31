bf9fa721628a5115a78eb6c0bcc10403807a61cf
Ziming Hua <113824411+hua7450@users.noreply.github.com>
2025-05-30 20:11:21 -0400

    minor
    Co-authored-by: Anthony V. <anth.volk@gmail.com>

--- src/data/staff.js
+++ src/data/staff.js
@@ -88,7 +88,7 @@ The Rochester Institute of Technology.`,
     bio: `is a policy modeling fellow at PolicyEngine. He focuses on
     implementing and analyzing tax and benefit systems. He develops simulation models
     to evaluate the impacts of policy reforms. Ziming holds a bachelor's degree in
-    Mathematical Finance from the University of California, Irvine, and a master's degree
+    mathematical finance from the University of California, Irvine, and a master's degree
     in Business Analytics from Boston University.`,
     image: require("../images/authors/ziming-hua.jpg"),
   },
